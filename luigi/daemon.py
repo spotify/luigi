39ea1635de93cc411ad78c7775e9acdf700cbd49
Erik Bernhardsson <erikbern@spotify.com>
2012-01-10 22:02:32 +0000

    added code not to run same process twice

---
+++ luigi/daemon.py
@@ -0,0 +1,53 @@
+import os, sys, hashlib
+
+def getpcmd(pid):
+    ''' Returns command of process
+    '''
+    cmd = 'ps -p %s -o cmd=' % (pid,)
+    p = os.popen(cmd, 'r')
+    return p.readline().strip()
+
+def run_once(pid_dir):
+    ''' Makes sure the process is only run once at the same time with the same name.
+
+    Notice that we since we check the process name, different parameters to the same
+    command can spawn multiple processes at the same time, i.e. running
+    "/usr/bin/my_process" does not prevent anyone from launching
+    "/usr/bin/my_process --foo bar".
+    '''
+
+    # Check the name and pid of this process
+    my_pid = os.getpid()
+    my_cmd = getpcmd(my_pid)
+
+    # Check if there is a pid file corresponding to this name
+    if not os.path.exists(pid_dir):
+        os.mkdir(pid_dir)
+
+    pidfile = os.path.join(pid_dir, hashlib.md5(my_cmd).hexdigest()) + '.pid'
+
+    if os.path.exists(pidfile):
+        # There is such a file - read the pid and look up its process name
+        try:
+            pid = int(open(pidfile).readline().strip())
+        except ValueError:
+            pid = -1
+
+        cmd = getpcmd(pid)
+
+        if cmd == my_cmd:
+            # We are already running under a different pid
+            print 'Pid', pid, 'running'
+            sys.exit(1)
+        else:
+            # The pid belongs to something else, we could 
+            pass
+
+    # Write pid
+    pid = os.getpid()
+    f = open(pidfile, 'w')
+    f.write('%d\n' % (pid, ))
+    f.close()
+
+    return True
+
