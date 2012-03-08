2a5c6e32e28458c7715909d20ed167b00fc49489
Elias Freider <freider@spotify.com>
2012-03-08 08:46:24 +0000

    More linting

--- luigi/target.py
+++ luigi/target.py
@@ -1,3 +1,6 @@
-class Target(object): # interface
-    def exists(self): raise NotImplementedError
-    def open(self, mode): raise NotImplementedError
+class Target(object):  # interface
+    def exists(self):
+        raise NotImplementedError
+
+    def open(self, mode):
+        raise NotImplementedError
