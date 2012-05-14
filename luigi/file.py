a2fc8da1e822c7f7546cf38aeccd6dc55fdb4260
Pablo Barrera <pablo@spotify.com>
2012-05-14 18:04:50 +0200

    LocalTarget and File create folder while creating a new file
    Change-Id: Ie8e2ba3bf18419ed11c40283799364b20e254753

--- luigi/file.py
+++ luigi/file.py
@@ -8,6 +8,12 @@ class File(object):
         return os.path.exists(self.__path)
 
     def open(self, mode = 'r'):
+        if mode == 'w' or mode == 'a':
+            # Create folder if it does not exist
+            normpath = os.path.normpath(self.__path)
+            parentfolder = os.path.dirname(normpath)
+            if not os.path.exists(parentfolder):
+                os.makedirs(parentfolder)
         return open(self.__path, mode)
     
     def remove(self):
