a8a2955d89a1fd3bea032fc9afd1b2c7d360d080
Anders Nyman <nyman@spotify.com>
2012-07-02 16:28:36 +0000

    Changing __path to path in order to enable running locally
    Change-Id: I68eed585c963485c99dc80b0a6630084ae2e54a8
    Reviewed-on: https://gerrit.spotify.net/gerrit/16284
    TC-Compile-And-Test: teamcity teamcity <teamcity@spotify.com>
    Reviewed-by: Elias Freider <freider@spotify.com>

--- luigi/file.py
+++ luigi/file.py
@@ -5,12 +5,12 @@ class atomic_file(file):
     # Also cleans up the temp file if close is not invoked
     def __init__(self, path):
         self.__tmp_path = path + '-luigi-tmp-%09d' % random.randrange(0, 999999999)
-        self.__path = path
+        self.path = path
         super(atomic_file, self).__init__(self.__tmp_path, 'w')
 
     def close(self):
         super(atomic_file, self).close()
-        os.rename(self.__tmp_path, self.__path)
+        os.rename(self.__tmp_path, self.path)
 
     def __del__(self):
         if os.path.exists(self.__tmp_path):
@@ -22,29 +22,29 @@ class atomic_file(file):
 
 class File(object):
     def __init__(self, path):
-        self.__path = path
+        self.path = path
 
     def exists(self):
-        return os.path.exists(self.__path)
+        return os.path.exists(self.path)
 
     def open(self, mode='r'):
         if mode == 'w':
             # Create folder if it does not exist
-            normpath = os.path.normpath(self.__path)
+            normpath = os.path.normpath(self.path)
             parentfolder = os.path.dirname(normpath)
             if not os.path.exists(parentfolder):
                 os.makedirs(parentfolder)
 
-            return atomic_file(self.__path)
+            return atomic_file(self.path)
 
         elif mode == 'r':
-            return open(self.__path, mode)
+            return open(self.path, mode)
         else:
             raise Exception('mode must be r/w')
-    
+
     def remove(self):
-    	os.remove(self.__path)
+        os.remove(self.path)
 
     @property
     def fn(self):
-        return self.__path
+        return self.path
