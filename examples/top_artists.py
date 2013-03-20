2cfacb64537e43f5ecd7df867952c27ad496d0d7
Erik Bernhardsson <erikbern@spotify.com>
2013-03-19 22:21:09 -0700

    Rewrote README.md and added the examples from the PyData 2013 preso by Elias
    RFC: let me know if this makes sense.
    
    Updated pictures to use the new visualizer, except the big splashy dep graph which I'll do later.
    
    Removed some crap in examples/ and doc/ and added some more random stuff to README.md

---
+++ examples/top_artists.py
@@ -0,0 +1,118 @@
+import random
+import luigi, luigi.hdfs, luigi.hadoop
+import luigi.postgres
+from heapq import nlargest
+from collections import defaultdict
+
+class ExternalStreams(luigi.ExternalTask):
+    ''' Example of a possible external data dump
+
+    To depend on external targets (typically at the top of your dependency grpah), you can define
+    an ExternalTask like this.
+    '''
+    date = luigi.DateParameter()
+
+    def output(self):
+        return luigi.hdfs.HdfsTarget(self.date.strftime('data/streams_%Y-%m-%d.tsv'))
+
+class Streams(luigi.Task):
+    ''' Faked version right now, just generates bogus data.    
+    '''
+    date = luigi.DateParameter()
+
+    def run(self):
+        f = self.output().open('w')
+        for i in xrange(1000):
+            print >> f, random.randint(0, 999), random.randint(0, 999), random.randint(0, 999)
+        f.close()
+
+    def output(self):
+        return luigi.LocalTarget(self.date.strftime('data/streams_%Y_%m_%d_faked.tsv'))
+
+class StreamsHdfs(Streams):
+    def output(self):
+        return luigi.HdfsTarget(self.date.strftime('data/streams_%Y_%m_%d_faked.tsv'))
+
+class AggregateArtists(luigi.Task):
+    date_interval = luigi.DateIntervalParameter()
+
+    def output(self):
+        return luigi.LocalTarget("data/artist_streams_%s.tsv" % self.date_interval)
+
+    def requires(self):
+        return [Streams(date) for date in self.date_interval]
+
+    def run(self):
+        artist_count = defaultdict(int)
+
+        for input in self.input():
+            with input.open('r') as in_file:
+                for line in in_file:
+                    timestamp, artist, track = line.strip().split()
+                    artist_count[artist] += 1
+
+        with self.output().open('w') as out_file:
+            for artist, count in artist_count.iteritems():
+                print >> out_file, artist, count
+
+class AggregateArtistsHadoop(luigi.hadoop.JobTask):
+    date_interval = luigi.DateIntervalParameter()
+
+    def output(self):
+        return luigi.HdfsTarget("data/artist_streams_%s.tsv" % self.date_interval)
+
+    def requires(self):
+        return [StreamsHdfs(date) for date in self.date_interval]
+
+    def mapper(self, line):
+        timestamp, artist, track = line.strip().split()
+        yield artist, 1
+        
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+class Top10Artists(luigi.Task):
+    date_interval = luigi.DateIntervalParameter()
+    use_hadoop = luigi.BooleanParameter()
+
+    def requires(self):
+        if self.use_hadoop:
+            return AggregateArtistsHadoop(self.date_interval)
+        else:
+            return AggregateArtists(self.date_interval)
+
+    def output(self):
+        return luigi.LocalTarget("data/top_artists_%s.tsv" % self.date_interval)
+
+    def run(self):
+        top_10 = nlargest(10, self._input_iterator())
+        with self.output().open('w') as out_file:
+            for streams, artist in top_10:
+                print >> out_file, self.date_interval.date_a, self.date_interval.date_b, artist, streams
+
+    def _input_iterator(self):
+        with self.input().open('r') as in_file:
+            for line in in_file:
+                artist, streams = line.strip().split()
+                yield int(streams), int(artist)
+
+class ArtistToplistToDatabase(luigi.postgres.CopyToTable):
+    date_interval = luigi.DateIntervalParameter()
+    use_hadoop = luigi.BooleanParameter()
+
+    host = "localhost"
+    database = "toplists"
+    user = "luigi"
+    password = "abc123"  # ;)
+    table = "top10"
+
+    columns = [("date_from", "DATE"),
+               ("date_to", "DATE"),
+               ("artist", "TEXT"),
+               ("streams", "INT")]
+
+    def requires(self):
+        return Top10Artists(self.date_interval, self.use_hadoop)
+
+if __name__ == "__main__":
+    luigi.run()
