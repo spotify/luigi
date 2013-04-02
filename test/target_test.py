699070c33d37bd456ea8b97f5f63415884b8e476
Joe Crobak <joe@foursquare.com>
2013-04-02 21:48:22 +0000

    introduce ABCMeta for Target and Task.
    Task's metaclass inherits ABCMeta and Target uses ABCMeta as its
    metaclass. This lets you define abstract targets and tasks that
    can't be instantiated directly.
    
    I have a few simple tests to demonstrate the expected usage and
    to make sure that things still work ok.
    
    Open question: Does it make sense for Target.open() to exist? It
    only seems to be applicable to HDFS and File Targets (maybe an
    OpenableTarget interface?). Or should we leave it without the
    @abstractmethod annotation.

---
+++ test/target_test.py
@@ -0,0 +1,31 @@
+import unittest
+
+import luigi.target
+
+
+class TargetTest(unittest.TestCase):
+    def test_cannot_instantiate(self):
+        def instantiate_target():
+            luigi.target.Target()
+
+        self.assertRaises(TypeError, instantiate_target)
+
+    def test_abstract_subclass(self):
+        class ExistsTarget(luigi.target.Target):
+            def exists(self):
+                return True
+
+        def instantiate_target():
+            ExistsTarget()
+
+        self.assertRaises(TypeError, instantiate_target)
+
+    def test_instantiate_subclass(self):
+        class GoodTarget(luigi.target.Target):
+            def exists(self):
+                return True
+
+            def open(self, mode):
+                return None
+
+        GoodTarget()
