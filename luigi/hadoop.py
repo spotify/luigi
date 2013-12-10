1b1d0b47fe39b18b01fa7b55b8bd01432247ba4c
Dave Buchfuhrer <dbuchfuhrer@gmail.com>
2013-12-10 14:21:45 -0800

    Generalizes exit code for captured signals

--- luigi/hadoop.py
+++ luigi/hadoop.py
@@ -157,8 +157,9 @@ class HadoopRunContext(object):
         if self.job_id:
             logger.info('Job interrupted, killing job %s', self.job_id)
             subprocess.call(['mapred', 'job', '-kill', self.job_id])
-        if captured_signal == signal.SIGTERM:
-            sys.exit(143)
+        if captured_signal is not None:
+            # adding 128 gives the exit code corresponding to a signal
+            sys.exit(128 + captured_signal)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_type is KeyboardInterrupt:
