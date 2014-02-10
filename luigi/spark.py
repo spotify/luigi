ee014a50795c20785cbb8bca84fbe50bdacc3fbc
Edward Newett <enewett@spotify.com>
2014-02-10 18:15:20 -0500

    Support for Apache Spark
    This patch adds basic support for launching a
    Spark application on YARN in standalone mode.
    
    - support for env specific config through client.cfg
    - simple status tracking / error handling
    - kills the yarn app on ctrl+c
    
    Change-Id: Ia1b488eef74c2bade31c9c3fc38b8e508d6cee64

---
+++ luigi/spark.py
@@ -0,0 +1,198 @@
+import datetime
+import logging
+import os
+import random
+import re
+import subprocess
+import signal
+import sys
+import tempfile
+import time
+
+import luigi
+import luigi.format
+import luigi.hdfs
+from luigi import configuration
+
+
+logger = logging.getLogger('luigi-interface')
+
+"""
+Apache Spark on YARN support
+
+Example configuration section in client.cfg:
+
+[spark]
+# assembly jar containing spark and dependencies
+spark-jar: /usr/share/spark/jars/spark-assembly-0.8.1-incubating-hadoop2.2.0.jar
+
+# spark script to invoke
+spark-class: /usr/share/spark/spark-class
+
+# directory containing the (client side) configuration files for the hadoop cluster
+hadoop-conf-dir: /etc/hadoop/conf
+
+"""
+
+
+class SparkRunContext(object):
+    def __init__(self):
+        self.app_id = None
+
+    def __enter__(self):
+        self.__old_signal = signal.getsignal(signal.SIGTERM)
+        signal.signal(signal.SIGTERM, self.kill_job)
+        return self
+
+    def kill_job(self, captured_signal=None, stack_frame=None):
+        if self.app_id:
+            done = False
+            while not done:
+                try:
+                    logger.info('Job interrupted, killing application %s', self.app_id)
+                    subprocess.call(['yarn', 'application', '-kill', self.app_id])
+                    done = True
+                except KeyboardInterrupt:
+                    continue
+
+        if captured_signal is not None:
+            # adding 128 gives the exit code corresponding to a signal
+            sys.exit(128 + captured_signal)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is KeyboardInterrupt:
+            self.kill_job()
+        signal.signal(signal.SIGTERM, self.__old_signal)
+
+
+class SparkJobError(RuntimeError):
+    def __init__(self, message, out=None, err=None):
+        super(SparkJobError, self).__init__(message, out, err)
+        self.message = message
+        self.out = out
+        self.err = err
+
+
+class SparkJob(luigi.Task):
+    spark_workers = None
+    spark_master_memory = None
+    spark_worker_memory = None
+    queue = luigi.Parameter(is_global=True, default=None, significant=False)
+    temp_hadoop_output_file = None
+
+    def requires_local(self):
+        ''' Default impl - override this method if you need any local input to be accessible in init() '''
+        return []
+
+    def requires_hadoop(self):
+        return self.requires()  # default impl
+
+    def input_local(self):
+        return luigi.task.getpaths(self.requires_local())
+
+    def input(self):
+        return luigi.task.getpaths(self.requires())
+
+    def deps(self):
+        # Overrides the default implementation
+        return luigi.task.flatten(self.requires_hadoop()) + luigi.task.flatten(self.requires_local())
+
+    def jar(self):
+        raise NotImplementedError("subclass should define jar containing job_class")
+
+    def job_class(self):
+        raise NotImplementedError("subclass should define Spark job_class")
+
+    def job_args(self):
+        return []
+
+    def output(self):
+        raise NotImplementedError("subclass should define HDFS output path")
+
+    def run(self):
+        original_output_path = self.output().path
+        path_no_slash = original_output_path[:-2] if original_output_path.endswith('/*') else original_output_path
+        path_no_slash = original_output_path[:-1] if original_output_path[-1] == '/' else path_no_slash
+        tmp_output = luigi.hdfs.HdfsTarget(path_no_slash + '-luigi-tmp-%09d' % random.randrange(0, 1e10))
+
+        args = ['org.apache.spark.deploy.yarn.Client']
+        args += ['--jar', self.jar()]
+        args += ['--class', self.job_class()]
+
+        for a in self.job_args():
+            if a == self.output().path:
+                # pass temporary output path to job args
+                logger.info("Using temp path: {0} for path {1}".format(tmp_output.path, original_output_path))
+                args += ['--args', tmp_output.path]
+            else:
+                args += ['--args', str(a)]
+
+        if self.spark_workers is not None:
+            args += ['--num-workers', self.spark_workers]
+
+        if self.spark_master_memory is not None:
+            args += ['--master-memory', self.spark_master_memory]
+
+        if self.spark_worker_memory is not None:
+            args += ['--worker-memory', self.spark_worker_memory]
+
+        queue = self.queue
+        if queue is not None:
+            args += ['--queue', queue]
+
+        env = os.environ.copy()
+        env['SPARK_JAR'] = configuration.get_config().get('spark', 'spark-jar')
+        env['HADOOP_CONF_DIR'] = configuration.get_config().get('spark', 'hadoop-conf-dir')
+        env['MASTER'] = 'yarn-client'
+        spark_class = configuration.get_config().get('spark', 'spark-class')
+
+        temp_stderr = tempfile.TemporaryFile()
+        logger.info('Running: %s %s' % (spark_class, ' '.join(args)))
+        proc = subprocess.Popen([spark_class] + args, stdout=subprocess.PIPE,
+                                stderr=temp_stderr, env=env, close_fds=True)
+
+        return_code, final_state, app_id = self.track_progress(proc)
+        if return_code == 0 and final_state != 'FAILED':
+            tmp_output.move(path_no_slash)
+        elif final_state == 'FAILED':
+            raise SparkJobError('Spark job failed: see yarn logs for %s' % app_id)
+        else:
+            temp_stderr.seek(0)
+            errors = temp_stderr.readlines()
+            logger.error(errors)
+            raise SparkJobError('Spark job failed', err=errors)
+
+    def track_progress(self, proc):
+        # The Spark client currently outputs a multiline status to stdout every second
+        # while the application is running.  This instead captures status data and updates
+        # a single line of output until the application finishes.
+        app_id = None
+        app_status = 'N/A'
+        url = 'N/A'
+        final_state = None
+        start = time.time()
+        with SparkRunContext() as context:
+            while proc.poll() is None:
+                s = proc.stdout.readline()
+                app_id_s = re.compile('application identifier: (\w+)').search(s)
+                if app_id_s:
+                    app_id = app_id_s.group(1)
+                    context.app_id = app_id
+                app_status_s = re.compile('yarnAppState: (\w+)').search(s)
+                if app_status_s:
+                    app_status = app_status_s.group(1)
+                url_s = re.compile('appTrackingUrl: (.+)').search(s)
+                if url_s:
+                    url = url_s.group(1)
+                final_state_s = re.compile('distributedFinalState: (\w+)').search(s)
+                if final_state_s:
+                    final_state = final_state_s.group(1)
+                if not app_id:
+                    logger.info(s.strip())
+                else:
+                    elapsed_mins, elapsed_secs = divmod(datetime.timedelta(seconds=time.time() - start).seconds, 60)
+                    status = '[%0d:%02d] Status: %s Tracking: %s' % (elapsed_mins, elapsed_secs, app_status, url)
+                    sys.stdout.write("\r\x1b[K" + status)
+                    sys.stdout.flush()
+        logger.info(proc.communicate()[0])
+        return proc.returncode, final_state, app_id
