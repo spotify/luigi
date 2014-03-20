a2b7ccc410a36d4d4a82568598c65af581501963
Joe Crobak <joecrow@gmail.com>
2014-03-20 01:43:55 +0000

    Switch to sphinx RTD theme.
    Conditionally add the sphinx RTD theme if we're not on RTD.

--- doc/conf.py
+++ doc/conf.py
@@ -106,7 +106,20 @@ autodoc_member_order = 'bysource'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    try:
+        import sphinx_rtd_theme
+    except ImportError:
+        raise Exception("You must `pip install sphinx_rtd_theme` to build docs locally.")
+
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
