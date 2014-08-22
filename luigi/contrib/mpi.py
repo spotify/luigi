f7af6d68afebe89e609a42dde3b54b67067e07e5
Dale Roberts <dale.o.roberts@gmail.com>
2014-08-22 15:20:58 +1000

    PEP8

--- luigi/contrib/mpi.py
+++ luigi/contrib/mpi.py
@@ -4,7 +4,7 @@ import logging
 
 from luigi import build
 from luigi.worker import Worker, Event
-from luigi.scheduler import *
+from luigi.scheduler import CentralPlannerScheduler, Scheduler
 
 atexit.register(pp.finalize)
 
@@ -24,23 +24,27 @@ class MPIScheduler(Scheduler):
         return pp.receive(source=with_rank)
 
     def add_task(self, *args, **kwargs):
-        return self._sndrcv({'cmd': 'add_task', 'args': args, 'kwargs': kwargs})
+        return self._sndrcv({'cmd': 'add_task', 'args': args,
+                             'kwargs': kwargs})
 
     def get_work(self, *args, **kwargs):
-        return self._sndrcv({'cmd': 'get_work', 'args': args, 'kwargs': kwargs})
+        return self._sndrcv({'cmd': 'get_work', 'args': args,
+                             'kwargs': kwargs})
 
     def ping(self, *args, **kwargs):
-        return self._sndrcv({'cmd': 'ping', 'args': args, 'kwargs': kwargs})
+        return self._sndrcv({'cmd': 'ping', 'args': args,
+                             'kwargs': kwargs})
 
 
 class MPIWorker(Worker):
 
-    def __init__(self, scheduler=MPIScheduler(), worker_id=None, ping_interval=None,
-                 keep_alive=None, wait_interval=None):
+    def __init__(self, scheduler=MPIScheduler(), worker_id=None,
+                 ping_interval=None, keep_alive=None, wait_interval=None):
         if not worker_id:
             worker_id = 'mpi-%06d' % pp.rank()
         super(MPIWorker, self).__init__(scheduler, worker_id=worker_id,
-                                        ping_interval=ping_interval, keep_alive=keep_alive,
+                                        ping_interval=ping_interval,
+                                        keep_alive=keep_alive,
                                         wait_interval=wait_interval)
         self.host = self.host + ':' + self._id
 
@@ -55,26 +59,27 @@ class MasterMPIWorker(MPIWorker):
         return self._task_status.setdefault(task.task_id, task.complete())
 
     def run(self):
-        # Here we block to wait for all workers to be ready for initialisation.
-        # At this stage the MasterMPIWorker object (i.e., self) has checked if
-        # tasks are already complete. This allows the `SlaveMPIWorker`s to query
-        # the `MasterMPIWorker` to see if the task is complete instead of thrashing
-        # the (distributed) file system.
-        
+        # Here we block to wait for all workers to be ready for
+        # initialisation.  At this stage the MasterMPIWorker object
+        # (i.e., self) has checked if tasks are already complete. This
+        # allows the `SlaveMPIWorker`s to query the `MasterMPIWorker` to
+        # see if the task is complete instead of thrashing the
+        # (distributed) file system.
+
         log.debug('Syncronising with slaves')
         pp.barrier()
 
         # Handle the messages from the workers
 
-        slaves_alive = pp.size() - 1 # minus the master
+        slaves_alive = pp.size() - 1  # minus the master
 
         while slaves_alive > 0:
             msg, status = pp.receive(source=pp.any_source, return_status=True)
             cmd, args, kwargs = msg['cmd'], msg['args'], msg['kwargs']
 
-            try: # to pass along the message to the master scheduler
+            try:  # to pass along the message to the master scheduler
                 func = getattr(self._scheduler, cmd)
-                result = func(*args, **kwargs)                
+                result = func(*args, **kwargs)
                 # send back the result
                 pp.send(result, status.source)
             except AttributeError:
@@ -93,12 +98,13 @@ class MasterMPIWorker(MPIWorker):
 
 class SlaveMPIWorker(MPIWorker):
 
-    def __init__(self, scheduler=MPIScheduler(), worker_id=None, ping_interval=None,
-                 keep_alive=None, wait_interval=None):
+    def __init__(self, scheduler=MPIScheduler(), worker_id=None,
+                 ping_interval=None, keep_alive=None, wait_interval=None):
 
-        # Here we block to allow the MasterMPIWorker to check the completion 
-        # status of all tasks (see `_check_complete`). This is to stop
-        # SlaveMPIWorkers from thrashing the (distributed) file system.
+        # Here we block to allow the MasterMPIWorker to check the
+        # completion status of all tasks (see `_check_complete`). This
+        # is to stop SlaveMPIWorkers from thrashing the (distributed)
+        # file system.
 
         log.debug('Syncronising with master')
         pp.barrier()
@@ -111,7 +117,8 @@ class SlaveMPIWorker(MPIWorker):
                                              wait_interval=wait_interval)
 
     def _check_complete(self, task):
-        pp.send({'cmd': 'check_complete', 'args': [task.task_id], 'kwargs': None},
+        pp.send({'cmd': 'check_complete', 'args': [task.task_id],
+                 'kwargs': None},
                 destination=0)
         return pp.receive(source=0)
 
@@ -131,7 +138,7 @@ class WorkerSchedulerFactory(object):
         if pp.rank() > 0:
             self.scheduler = MPIScheduler()
             self.worker = SlaveMPIWorker(self.scheduler)
-        else: # on master
+        else:  # on master
             self.scheduler = MasterScheduler(task_history=task_history)
             self.worker = MasterMPIWorker(self.scheduler)
 
@@ -143,6 +150,7 @@ class WorkerSchedulerFactory(object):
 
     def create_worker(self, scheduler, worker_processes=None):
         return self.worker
+
 
 def run(tasks, task_history=None):
     sch = WorkerSchedulerFactory(task_history=task_history)
