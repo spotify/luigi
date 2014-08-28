dd808322d134c692bb0b37a968d142d5f108b140
Erik Bernhardsson <erikbern@spotify.com>
2014-08-28 13:49:16 -0400

    Revert "Gracefully close worker on term."

--- luigi/worker.py
+++ luigi/worker.py
@@ -101,8 +101,6 @@ class Worker(object):
         self.run_succeeded = True
         self.unfulfilled_counts = collections.defaultdict(int)
 
-        self.graceful_shutdown_initiated = False
-
         class KeepAliveThread(threading.Thread):
             """ Periodically tell the scheduler that the worker still lives """
             def __init__(self):
@@ -140,14 +138,6 @@ class Worker(object):
         """
         self._keep_alive_thread.stop()
         self._keep_alive_thread.join()
-
-    def graceful_shutdown(self):
-        """
-        Call this method to tell worker to finish tasks that it is running
-        and shutdown without accepting any new ones
-        """
-        logger.info("Worker %s shutting down gracefully" % self._id)
-        self.graceful_shutdown_initiated = True
 
     def _generate_worker_info(self):
         # Generate as much info as possible about the worker
@@ -404,10 +394,7 @@ class Worker(object):
             while len(children) >= self.worker_processes:
                 self._reap_children(children)
 
-            if not self.graceful_shutdown_initiated:
-                task_id, running_tasks, n_pending_tasks = self._get_work()
-            else:
-                task_id, running_tasks, n_pending_tasks = [None, None, None]
+            task_id, running_tasks, n_pending_tasks = self._get_work()
 
             if task_id is None:
                 self._log_remote_tasks(running_tasks, n_pending_tasks)
