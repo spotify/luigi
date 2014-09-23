1c96bafc80760286e8bdeac887c89136f8067ff1
Vishal Patel <vpatel@theretailequation.com>
2014-09-23 10:34:43 -0700

    Updated the doc string of the hiverc function.

--- luigi/hive.py
+++ luigi/hive.py
@@ -237,6 +237,7 @@ class HiveQueryTask(luigi.hadoop.BaseHadoopJobTask):
         """ Location of an rc file to run before the query
             if hiverc-location key is specified in client.cfg, will default to the value there
             otherwise returns None
+            Returning a list of rc files will load all of them in order.
         """
         return luigi.configuration.get_config().get('hive', 'hiverc-location', default=None)
 
