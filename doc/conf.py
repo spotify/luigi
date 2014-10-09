61f08705e1098058ad19498404762bb42d907a18
mrchoey <choey@mortardata.com>
2014-10-09 16:26:42 -0400

    update rtd config to fix broken autodoc builds https://readthedocs.org/builds/luigi/1706083/

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath(os.path.pardir))
+sys.path.insert(0, os.path.abspath('../api/'))
 
 # append the __init__ to class definitions
 autoclass_content = 'both'
