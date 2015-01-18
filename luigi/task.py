fc560f3075e22f71b96d73ecad76c78a52a376d3
Erik Bernhardsson <erikbern@spotify.com>
2015-01-18 00:19:55 -0500

    Make it possible to override global parameters

--- luigi/task.py
+++ luigi/task.py
@@ -353,8 +353,6 @@ class Task(object):
                 raise parameter.DuplicateParameterException('%s: parameter %s was already set as a positional parameter' % (exc_desc, param_name))
             if param_name not in params_dict:
                 raise parameter.UnknownParameterException('%s: unknown parameter %s' % (exc_desc, param_name))
-            if params_dict[param_name].is_global:
-                raise parameter.ParameterException('%s: can not override global parameter %s' % (exc_desc, param_name))
             result[param_name] = arg
 
         # Then use the defaults for anything not filled in
