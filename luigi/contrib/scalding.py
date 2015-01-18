aee679db09c536641407e23be3edcc9aaddca5cd
Erik Bernhardsson <erikbern@spotify.com>
2015-01-18 16:26:30 -0500

    additional documentation fix for scalding

--- luigi/contrib/scalding.py
+++ luigi/contrib/scalding.py
@@ -15,27 +15,28 @@ logger = logging.getLogger('luigi-interface')
 """
 Scalding support for Luigi.
 
-Example configuration section in client.cfg:
-[scalding]
-# scala home directory, which should include a lib subdir with scala jars.
-scala-home: /usr/share/scala
+Example configuration section in client.cfg::
 
-# scalding home directory, which should include a lib subdir with
-# scalding-*-assembly-* jars as built from the official Twitter build script.
-scalding-home: /usr/share/scalding
+    [scalding]
+    # scala home directory, which should include a lib subdir with scala jars.
+    scala-home: /usr/share/scala
 
-# provided dependencies, e.g. jars required for compiling but not executing
-# scalding jobs. Currently requred jars:
-# org.apache.hadoop/hadoop-core/0.20.2
-# org.slf4j/slf4j-log4j12/1.6.6
-# log4j/log4j/1.2.15
-# commons-httpclient/commons-httpclient/3.1
-# commons-cli/commons-cli/1.2
-# org.apache.zookeeper/zookeeper/3.3.4
-scalding-provided: /usr/share/scalding/provided
+    # scalding home directory, which should include a lib subdir with
+    # scalding-*-assembly-* jars as built from the official Twitter build script.
+    scalding-home: /usr/share/scalding
 
-# additional jars required.
-scalding-libjars: /usr/share/scalding/libjars
+    # provided dependencies, e.g. jars required for compiling but not executing
+    # scalding jobs. Currently requred jars:
+    # org.apache.hadoop/hadoop-core/0.20.2
+    # org.slf4j/slf4j-log4j12/1.6.6
+    # log4j/log4j/1.2.15
+    # commons-httpclient/commons-httpclient/3.1
+    # commons-cli/commons-cli/1.2
+    # org.apache.zookeeper/zookeeper/3.3.4
+    scalding-provided: /usr/share/scalding/provided
+
+    # additional jars required.
+    scalding-libjars: /usr/share/scalding/libjars
 """
 
 
