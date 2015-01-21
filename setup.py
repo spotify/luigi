c4e63512a987ffdabdf3e42b29f0535a03a19d99
Fairiz 'Fi' Azizi <coderfi@users.noreply.github.com>
2015-01-20 16:52:03 -0800

    reverted version to upstream's 1.0.19 version

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ if sys.version_info[:2] < (2, 7):
 
 setup(
     name='luigi',
-    version='1.0.19.1',
+    version='1.0.19',
     description='Workflow mgmgt + task scheduling + dependency resolution',
     long_description=long_description,
     author='Erik Bernhardsson',
