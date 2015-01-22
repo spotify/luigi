bf8cbaf0bdd9a6f0b16be050bd6b6fb060426009
Alexander Krasnukhin <the.malkolm@gmail.com>
2015-01-22 19:15:58 +0100

    Extract deprecate_kwarg to a separate file.
    Otherwise we can't use it in luigi.parameter as instantly get
    circular dependencies i.e.
    
    luigi.parameter imports luigi.util.deprecate_kwarg
    luigi.util imports luigi.parameter (for previous() function)

--- luigi/util.py
+++ luigi/util.py
@@ -18,6 +18,7 @@ import datetime
 from luigi import parameter
 import task
 import functools
+from luigi.deprecate_kwarg import deprecate_kwarg
 
 
 logger = logging.getLogger('luigi-interface')
@@ -178,39 +179,6 @@ def delegates(task_that_delegates):
             task_that_delegates.run(self)
 
     return Wrapped
-
-
-def deprecate_kwarg(old_name, new_name, kw_value):
-    """ Rename keyword arguments, but keep backwards compatibility.
-
-    Usage:
-
-    >>> @deprecate_kwarg('old', 'new', 'defval')
-    ... def some_func(old='defval'):
-    ...     print(old)
-    ...
-    >>> some_func(new='yay')
-    yay
-    >>> some_func(old='yaay')
-    yaay
-    >>> some_func()
-    defval
-
-    """
-    def real_decorator(function):
-        def new_function(*args, **kwargs):
-            value = kw_value
-            if old_name in kwargs:
-                warnings.warn('Keyword argument {0} is deprecated, use {1}'
-                              .format(old_name, new_name))
-                value = kwargs[old_name]
-            if new_name in kwargs:
-                value = kwargs[new_name]
-                del kwargs[new_name]
-            kwargs[old_name] = value
-            return function(*args, **kwargs)
-        return new_function
-    return real_decorator
 
 
 def previous(task):
