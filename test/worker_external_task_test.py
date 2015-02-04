825b91bb8eff5c139f0aa43e959be2d6a46eace1
Al B <al@ACAC0E64.ipt.aol.com>
2015-02-04 11:35:31 +0000

    Create a simple test that tests the mocks

--- test/worker_external_task_test.py
+++ test/worker_external_task_test.py
@@ -17,30 +17,23 @@ import unittest
 from mock import Mock
 from helpers import with_config
 
-mock_external_task_target = Mock(spec=luigi.Target)
-mock_external_task_target.task_id = 'mock_external_task_target'
-mock_external_task_target.exists.return_value = True
-
-mock_external_task = Mock(spec=luigi.ExternalTask)
-mock_external_task.task_id = 'mock_external_task'
-mock_external_task.deps = lambda : []
-mock_external_task.disabled = False
-mock_external_task.complete.side_effect = [True, True, False, False, True, True]
-mock_external_task.output.return_value = mock_external_task_target
 
 test_task_has_run = False
+
 
 class TestTask(luigi.Task):
     """
     Requires a single file dependency
     """
-    def __init__(self):
-        super(TestTask, self).__init__()
+    external_task = luigi.Parameter()
+
+    def __init__(self, *args, **kwargs):
+        super(TestTask, self).__init__(*args, **kwargs)
         global test_task_has_run
         test_task_has_run = False
-    
+
     def requires(self):
-        return mock_external_task
+        return self.external_task
 
     def output(self):
         mock_target = Mock(spec=luigi.Target)
@@ -55,10 +48,24 @@ class TestTask(luigi.Task):
 
 class WorkerExternalTaskTest(unittest.TestCase):
 
+    def _build_mocks(self, external_task_completion):
+        mock_external_task_target = Mock(spec=luigi.Target)
+        mock_external_task_target.task_id = 'mock_external_task_target'
+        mock_external_task_target.exists.return_value = True
+
+        mock_external_task = Mock(spec=luigi.ExternalTask)
+        mock_external_task.task_id = 'mock_external_task'
+        mock_external_task.deps = lambda : []
+        mock_external_task.disabled = False
+        mock_external_task.complete.side_effect = external_task_completion
+        mock_external_task.output.return_value = mock_external_task_target
+
+        return mock_external_task
+
     @with_config({'core': {'retry-external-tasks': 'true',
                            'disable-num-failures': '2',
                            'retry-delay': '0.001'}})
-    def test_external_dependency_completes_later_successfully(self):
+    def test_external_dependency_already_complete(self):
         """
         Test that an external dependency that is not `complete` when luigi is invoked, but \
         becomes `complete` while the workflow is executing is re-evaluated.
@@ -67,12 +74,42 @@ class WorkerExternalTaskTest(unittest.TestCase):
                                                            'retry-external-tasks',
                                                            False) == True
 
-        test_task = TestTask()
+        # `complete` is called twice per iteration, so duplicate each return code. The
+        # following corresponds to 3 iterations - True.
+        mock_external_task = self._build_mocks(external_task_completion=[True, True])
+
+        test_task = TestTask(external_task=mock_external_task)
         luigi.build([test_task], local_scheduler=True)
 
         # assert mock_external_task.runnable == True
         assert test_task_has_run == True
         assert mock_external_task.complete.call_count == 2
+
+
+    # @with_config({'core': {'retry-external-tasks': 'true',
+    #                        'disable-num-failures': '2',
+    #                        'retry-delay': '0.001'}})
+    # def test_external_dependency_completes_later_successfully(self):
+    #     """
+    #     Test that an external dependency that is not `complete` when luigi is invoked, but \
+    #     becomes `complete` while the workflow is executing is re-evaluated.
+    #     """
+    #     assert luigi.configuration.get_config().getboolean('core',
+    #                                                        'retry-external-tasks',
+    #                                                        False) == True
+    #
+    #     # `complete` is called twice per iteration, so duplicate each return code. The
+    #     # following corresponds to 3 iterations - False, False, True.
+    #     mock_external_task = self._build_mocks(external_task_completion=[False, False,
+    #                                                                      False, False,
+    #                                                                      True, True])
+    #
+    #     test_task = TestTask()
+    #     luigi.build([test_task], local_scheduler=True)
+    #
+    #     # assert mock_external_task.runnable == True
+    #     assert test_task_has_run == True
+    #     assert mock_external_task.complete.call_count == 6
 
 
 if __name__ == '__main__':
