a6d6e1294bbed94938387bdc5ad76be7a3508120
Erik Bernhardsson <erikbern@spotify.com>
2015-02-11 07:00:30 -0500

    bumped version to 1.0.24

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ if sys.version_info[:2] < (2, 7):
 
 setup(
     name='luigi',
-    version='1.0.23',
+    version='1.0.24',
     description='Workflow mgmgt + task scheduling + dependency resolution',
     long_description=long_description,
     author='Erik Bernhardsson',
