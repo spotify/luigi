0fb73884232eff1ee81fba84fbd53713d44d40bc
Erik Bernhardsson <erikbern@spotify.com>
2015-02-18 06:38:12 -0500

    added 3.3 and 3.4 to list of compatible python versions

--- setup.py
+++ setup.py
@@ -80,6 +80,8 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: System :: Monitoring',
     ],
 )
