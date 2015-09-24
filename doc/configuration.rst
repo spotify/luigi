8e9b439ce93d75a9e162a2e4289b04f3e1ad7e7e
Arash Rouhani <arash@spotify.com>
2015-09-24 19:58:13 +0200

    Add semantic exit codes
    This commits adds a long requested feature, to be able to tell what the luigi
    client did (or didn't) based on its exit code. This closes spotify/luigi#687.

--- doc/configuration.rst
+++ doc/configuration.rst
@@ -411,6 +411,47 @@ hive resources and 1 mysql resource:
 Note that it was not necessary to specify the 1 for mysql here, but it
 is good practice to do so when you have a fixed set of resources.
 
+.. _retcode-config:
+
+[retcode]
+----------
+
+Configure return codes for the luigi binary. In the case of multiple return
+codes that could apply, for example a failing task and missing data, the
+*numerically greatest* return code is returned.
+
+We recommend that you copy this set of exit codes to your ``luigi.cfg`` file:
+
+::
+
+  [retcode]
+  # The following return codes are the recommended exit codes for luigi
+  # They are in increasing level of severity (for most applications)
+  already_running: 10
+  missing_data: 20
+  task_failed: 30
+  unhandled_exception: 40
+
+unhandled_exception
+  For exceptions during scheduling (if you raise from the ``complete()`` or
+  ``requires()`` methods for instance) or for internal luigi errors.  Defaults
+  to 4, since this type of error probably will not recover over time.
+missing_data
+  For when an :py:class:`~luigi.task.ExternalTask` is not complete, and this
+  caused the worker to give up.  As an alternative to fiddling with this, see
+  the [worker] keep_alive option.
+task_failed
+  For signaling that there were last known to have failed. Typically because
+  some exception have been raised.
+already_running
+  This can happen in two different cases. Either the local lock file was taken
+  at the time the invocation starts up. Or, the central scheduler have reported
+  that some tasks could not have been run, because other workers are already
+  running the tasks.
+
+If you customize return codes, prefer to set them in range 128 to 255 to avoid
+conflicts. Return codes in range 0 to 127 are reserved for possible future use
+by Luigi contributors.
 
 [scalding]
 ----------
---
+++ test/retcodes_test.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015-2015 Spotify AB
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from helpers import LuigiTestCase, with_config
+import mock
+import luigi
+import luigi.scheduler
+from luigi.cmdline import luigi_run
+
+
+class RetcodesTest(LuigiTestCase):
+
+    def run_and_expect(self, joined_params, retcode, extra_args=['--local-scheduler', '--no-lock']):
+        with self.assertRaises(SystemExit) as cm:
+            luigi_run((joined_params.split(' ') + extra_args))
+        self.assertEqual(cm.exception.code, retcode)
+
+    def run_with_config(self, retcode_config, *args, **kwargs):
+        with_config(dict(retcode=retcode_config))(self.run_and_expect)(*args, **kwargs)
+
+    def test_task_failed(self):
+        class FailingTask(luigi.Task):
+            def run(self):
+                raise ValueError()
+
+        self.run_and_expect('FailingTask', 0)  # Test default value to be 0
+        self.run_and_expect('FailingTask --retcode-task-failed 5', 5)
+        self.run_with_config(dict(task_failed='3'), 'FailingTask', 3)
+
+    def test_missing_data(self):
+        class MissingDataTask(luigi.ExternalTask):
+            def complete(self):
+                return False
+
+        self.run_and_expect('MissingDataTask', 0)  # Test default value to be 0
+        self.run_and_expect('MissingDataTask --retcode-missing-data 5', 5)
+        self.run_with_config(dict(missing_data='3'), 'MissingDataTask', 3)
+
+    def test_already_running(self):
+        class AlreadyRunningTask(luigi.Task):
+            def run(self):
+                pass
+
+        old_func = luigi.scheduler.CentralPlannerScheduler.get_work
+
+        def new_func(*args, **kwargs):
+            old_func(*args, **kwargs)
+            res = old_func(*args, **kwargs)
+            res['running_tasks'][0]['worker'] = "not me :)"  # Otherwise it will be filtered
+            return res
+
+        with mock.patch('luigi.scheduler.CentralPlannerScheduler.get_work', new_func):
+            self.run_and_expect('AlreadyRunningTask', 0)  # Test default value to be 0
+            self.run_and_expect('AlreadyRunningTask --retcode-already-running 5', 5)
+            self.run_with_config(dict(already_running='3'), 'AlreadyRunningTask', 3)
+
+    def test_when_locked(self):
+        def new_func(*args, **kwargs):
+            return False
+
+        with mock.patch('luigi.lock.acquire_for', new_func):
+            self.run_and_expect('Task', 0, extra_args=['--local-scheduler'])
+            self.run_and_expect('Task --retcode-already-running 5', 5, extra_args=['--local-scheduler'])
+            self.run_with_config(dict(already_running='3'), 'Task', 3, extra_args=['--local-scheduler'])
+
+    def test_unhandled_exception(self):
+        self.run_and_expect('UnknownTask', 4)
+        self.run_and_expect('UnknownTask --retcode-unhandled-exception 2', 2)
+
+        class TaskWithRequiredParam(luigi.Task):
+            param = luigi.Parameter()
+
+        self.run_and_expect('TaskWithRequiredParam --param hello', 0)
+        self.run_and_expect('TaskWithRequiredParam', 4)
+
+    def test_when_mixed_errors(self):
+
+        class FailingTask(luigi.Task):
+            def run(self):
+                raise ValueError()
+
+        class MissingDataTask(luigi.ExternalTask):
+            def complete(self):
+                return False
+
+        class RequiringTask(luigi.Task):
+            def requires(self):
+                yield FailingTask()
+                yield MissingDataTask()
+
+        self.run_and_expect('RequiringTask --retcode-task-failed 4 --retcode-missing-data 5', 5)
+        self.run_and_expect('RequiringTask --retcode-task-failed 7 --retcode-missing-data 6', 7)
+
+    def test_keyboard_interrupts(self):
+
+        class KeyboardInterruptTask(luigi.Task):
+            def run(self):
+                raise KeyboardInterrupt()
+
+        self.assertRaises(KeyboardInterrupt, luigi_run, ['KeyboardInterruptTask', '--local-scheduler', '--no-lock'])
