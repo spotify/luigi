32a4509d1aca43abbf77d62324a6fe82ad15904c
Arash Rouhani <arash@spotify.com>
2015-10-23 13:32:57 +0200

    readthedocs: Dont require python-daemon for rtd
    See issues in spotify/luigi#1342

--- setup.py
+++ setup.py
@@ -42,8 +42,10 @@ install_requires = [
 ]
 
 if os.environ.get('READTHEDOCS', None) == 'True':
-    install_requires.append('sqlalchemy')
     # So that we can build documentation for luigi.db_task_history and luigi.contrib.sqla
+    install_requires.append('sqlalchemy')
+    # readthedocs don't like python-daemon, see #1342
+    install_requires.remove('python-daemon<3.0')
 
 setup(
     name='luigi',
