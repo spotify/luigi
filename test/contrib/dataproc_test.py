43b41f41561def6db59d7d9e3b03c98ce1694ce8
Constantijn Visinescu <cvisinescu@xebia.com>
2016-03-18 16:40:18 +0100

    Luigi tasks for google dataproc

---
+++ test/contrib/dataproc_test.py
@@ -0,0 +1,132 @@
+"""This is an integration test for the Dataproc-luigi binding.
+
+This test requires credentials that can access GCS & access to a bucket below.
+Follow the directions in the gcloud tools to set up local credentials.
+"""
+import unittest
+
+try:
+    import oauth2client
+    import httplib2
+    from luigi.contrib import dataproc
+    from googleapiclient import discovery
+
+    default_credentials = oauth2client.client.GoogleCredentials.get_application_default()
+    default_client = discovery.build('dataproc', 'v1', credentials=default_credentials, http=httplib2.Http())
+    dataproc.set_dataproc_client(default_client)
+except ImportError:
+    raise unittest.SkipTest('Unable to load google cloud dependencies')
+
+import luigi
+import os
+import time
+from nose.plugins.attrib import attr
+
+# In order to run this test, you should set these to your GCS project.
+# Unfortunately there's no mock
+PROJECT_ID = os.environ.get('DATAPROC_TEST_PROJECT_ID', 'your_project_id_here')
+CLUSTER_NAME = os.environ.get('DATAPROC_TEST_CLUSTER', 'unit-test-cluster')
+REGION = os.environ.get('DATAPROC_REGION', 'global')
+
+
+class _DataprocBaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+class MySparkDataprocTask(dataproc.DataprocTask):
+    def run(self):
+        self.submit_spark_job(main_class="my.MainClass",
+                              jars=["one.jar", "two.jar"],
+                              job_args=["foo", "bar"])
+        self.wait_for_job()
+
+
+class MyPysparkDataprocTask(dataproc.DataprocTask):
+    def run(self):
+        self.submit_pyspark_job(job_file="main_job.py",
+                                extra_files=["extra1.py", "extra2.py"],
+                                job_args=["foo", "bar"])
+        self.wait_for_job()
+
+
+@attr('gcloud')
+class DataprocTaskTest(_DataprocBaseTestCase):
+
+    def test_1_create_cluster(self):
+        success = luigi.run(['--local-scheduler',
+                             '--no-lock',
+                             'CreateDataprocClusterTask',
+                             '--gcloud-project-id=' + PROJECT_ID,
+                             '--dataproc-cluster-name=' + CLUSTER_NAME])
+        self.assertTrue(success)
+
+    def test_2_create_cluster_should_notice_existing_cluster_and_return_immediately(self):
+        job_start = time.time()
+        success = luigi.run(['--local-scheduler',
+                             '--no-lock',
+                             'CreateDataprocClusterTask',
+                             '--gcloud-project-id=' + PROJECT_ID,
+                             '--dataproc-cluster-name=' + CLUSTER_NAME])
+        self.assertTrue(success)
+        self.assertLess(time.time() - job_start, 3)
+
+    def test_3_submit_spark_job(self):
+        # The job itself will fail because the python files don't exist
+        # We don't care, because then we would be testing spark
+        # We care the job was submitted correctly, so that's what we test
+
+        luigi.run(['--local-scheduler',
+                   '--no-lock',
+                   'MySparkDataprocTask',
+                   '--gcloud-project-id=' + PROJECT_ID,
+                   '--dataproc-cluster-name=' + CLUSTER_NAME])
+
+        response = dataproc.get_dataproc_client().projects().regions().jobs() \
+            .list(projectId=PROJECT_ID, region=REGION, clusterName=CLUSTER_NAME).execute()
+        lastJob = response['jobs'][0]
+
+        self.assertEquals(lastJob['sparkJob']['mainClass'], "my.MainClass")
+        self.assertEquals(lastJob['sparkJob']['jarFileUris'], ["one.jar", "two.jar"])
+        self.assertEquals(lastJob['sparkJob']['args'], ["foo", "bar"])
+
+    def test_4_submit_pyspark_job(self):
+        # The job itself will fail because the python files don't exist
+        # We don't care, because then we would be testing pyspark
+        # We care the job was submitted correctly, so that's what we test
+
+        luigi.run(['--local-scheduler',
+                   '--no-lock',
+                   'MyPysparkDataprocTask',
+                   '--gcloud-project-id=' + PROJECT_ID,
+                   '--dataproc-cluster-name=' + CLUSTER_NAME])
+
+        response = dataproc.get_dataproc_client().projects().regions().jobs()\
+            .list(projectId=PROJECT_ID, region=REGION, clusterName=CLUSTER_NAME).execute()
+        lastJob = response['jobs'][0]['pysparkJob']
+
+        self.assertEquals(lastJob['mainPythonFileUri'], "main_job.py")
+        self.assertEquals(lastJob['pythonFileUris'], ["extra1.py", "extra2.py"])
+        self.assertEquals(lastJob['args'], ["foo", "bar"])
+
+    def test_5_delete_cluster(self):
+        success = luigi.run(['--local-scheduler',
+                             '--no-lock',
+                             'DeleteDataprocClusterTask',
+                             '--gcloud-project-id=' + PROJECT_ID,
+                             '--dataproc-cluster-name=' + CLUSTER_NAME])
+        self.assertTrue(success)
+
+    def test_6_delete_cluster_should_return_immediately_if_no_cluster(self):
+        job_start = time.time()
+        success = luigi.run(['--local-scheduler',
+                             '--no-lock',
+                             'DeleteDataprocClusterTask',
+                             '--gcloud-project-id=' + PROJECT_ID,
+                             '--dataproc-cluster-name=' + CLUSTER_NAME])
+        self.assertTrue(success)
+        self.assertLess(time.time() - job_start, 3)
