345cb15c79589a39b9351dc425a24d83fcfd672a
Dillon Stadther <dlstadther@gmail.com>
2016-04-13 07:32:40 -0400

    Update test which uses logging_conf_file to default its value to an empty string rather than None.

--- test/cmdline_test.py
+++ test/cmdline_test.py
@@ -133,9 +133,9 @@ class CmdlineTest(unittest.TestCase):
     @mock.patch("luigi.interface.setup_interface_logging")
     def test_cmdline_logger(self, setup_mock, warn):
         with mock.patch("luigi.interface.core") as env_params:
-            env_params.return_value.logging_conf_file = None
+            env_params.return_value.logging_conf_file = ''
             luigi.run(['SomeTask', '--n', '7', '--local-scheduler', '--no-lock'])
-            self.assertEqual([mock.call(None)], setup_mock.call_args_list)
+            self.assertEqual([mock.call('')], setup_mock.call_args_list)
 
         with mock.patch("luigi.configuration.get_config") as getconf:
             getconf.return_value.get.side_effect = ConfigParser.NoOptionError(section='foo', option='bar')
