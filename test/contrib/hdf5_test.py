788b35ad3bac3d1f51a238d52892a9092c913a4e
kayibal <alan.f.hoeng@gmail.com>
2016-05-24 04:59:16 +0200

    Installing libhdf5 in travis.yaml
    Inititate branch to get travis to build pytables using pip
    
    Inititate branch to get travis to build pytables using pip
    
    trying installin pytables via apt-get
    
    try install manually from repository
    
    Install outside of tox
    
    Install outside of tox
    
    Add new matrix entry for this test
    
    Add py35 matrix entry for this test
    
    add attr to select test for hdf5
    
    Removed all docstrings
    
    small fix in test
    
    Updated Hdf5 target
    
    Fix import bug
    
    Enabling more tests
    
    Small fix
    
    Fixed flake errors
    
    More fixes on test
    
    skip tests using subtest for python2.7
    
    Fix unittest.skipIf definition
    
    Fiy python3.3 cython error

--- test/contrib/hdf5_test.py
+++ test/contrib/hdf5_test.py
@@ -1,144 +1,272 @@
 import os
 import tempfile
-import shutil
-import numpy as np
-import pandas.util.testing as pdt
-import pandas as pd
 import unittest
-from datetime import datetime
 
-from luigi.contrib.hdf5 import Hdf5TableTarget, Hdf5RowTarget, SafeHDFStore
+from luigi.contrib.hdf5 import Hdf5TableTarget, AtomicHdf5Table
+from nose.plugins.attrib import attr
+
+try:
+    import numpy as np
+    import pandas.util.testing as pdt
+    import pandas as pd
+except ImportError:
+    unittest.SkipTest("unable to load pandas module")
 
 
-class Hdf5TargetTest(unittest.TestCase):
+def cols_sorted(df):
+    l = list(df.columns)
+    return all([l[i] <= l[i + 1] for i in range(len(l) - 1)])
+
+
+@attr("hdf5")
+class Hdf5TableTargetTest(unittest.TestCase):
     def setUp(self):
+        self.existing = tempfile.mktemp()
+        self.new = tempfile.mktemp()
+
+        # create some data
         n = 10000
+        self.nrows = n
         lvl1 = np.random.choice(["some", "really", "random", "stuff", "not", "very", "creative"], size=n)
         lvl2 = np.random.choice(["well", "some", "more", "levels", "could", "be", "nice"], size=n)
         pids = list(map(lambda x: ".".join(x), zip(lvl1, lvl2)))
-        self.df = pd.DataFrame(dict(date=pd.date_range('20130101', periods=10000, freq="H"),
-                                    page_id=pids,
-                                    id=np.random.choice(np.arange(10), size=10000)))
-        self.dir = tempfile.mkdtemp()
-        self.hdf5_file_empty = os.path.join(self.dir, "hdf5-luigi-target-test-write.h5")
+        df = pd.DataFrame(dict(date=pd.date_range('20130101', periods=10000, freq="H"),
+                               page_id=pids,
+                               id=np.random.choice(np.arange(10), size=10000)))
+        self.data = pd.get_dummies(df, columns=["page_id"])
 
-        self.hdf5_file = os.path.join(self.dir, "hdf5-luigi-target-test-read.h5")
-        with SafeHDFStore(self.hdf5_file, "w") as store:
-            store.put("/df", self.df, complevel=1, complib="blosc", data_columns=True, format="table", append=True)
+        self.data.to_hdf(self.existing, key="/df")
 
-    def testTableTargetExists(self):
-        correct_true = Hdf5TableTarget(self.hdf5_file, "/df").exists()
-        self.assertTrue(correct_true, "Target exists method returned false for existing target")
+    def test_exists(self):
+        target = Hdf5TableTarget(self.existing, key="df")
+        self.assertTrue(target.exists())
 
-        correct_false = Hdf5TableTarget(self.hdf5_file_empty, "/df").exists()
-        self.assertFalse(correct_false, "Non existing file but claimed target exists")
+        target = Hdf5TableTarget(self.new, key="df")
+        self.assertFalse(target.exists())
 
-        correct_false = Hdf5TableTarget(self.hdf5_file, "/random").exists()
-        self.assertFalse(correct_false, "Non existing group but claimed target exists")
+        with target.open("w") as tb:
+            tb.write(self.data)
+        self.assertTrue(target.exists())
 
-    def testRowTargetExists(self):
-        print("Runinng target test on existing rows")
-        correct_true = Hdf5RowTarget(self.hdf5_file, "/df",
-                                     row_expr=pd.Term("(date > 20130101) & (date < 20140101)")).exists()
-        self.assertTrue(correct_true, "Got existent rows but claimed to be non existent")
+    def test_exists_splitted(self):
+        target = Hdf5TableTarget(self.new, key="df", split=2)
+        self.assertFalse(target.exists())
 
-        print("Checking if existing group is recognized")
-        correct_true = Hdf5RowTarget(self.hdf5_file, "/df").exists()
-        self.assertTrue(correct_true, "Got no row_expr and target exists, but claims to be non existent")
+        with target.open("w") as tb:
+            tb.write(self.data)
+        self.assertTrue(target.exists())
 
-        print("Checking if missing file returns False")
-        correct_false = Hdf5RowTarget(self.hdf5_file_empty, "/df").exists()
-        self.assertFalse(correct_false)
-        #
-        print("Checking if non existent group returns missing")
-        correct_false = Hdf5RowTarget(self.hdf5_file, "/test").exists()
-        self.assertFalse(correct_false)
+    def test_read(self):
+        target = Hdf5TableTarget(self.existing, key="df")
+        df = target.open().read(autoclose=1)
+        pdt.assert_frame_equal(df, self.data)
 
-        print("Checking if non existing rows return False")
-        correct_false = Hdf5RowTarget(self.hdf5_file, "/df", row_expr=pd.Term("date > 20410101")).exists()
-        self.assertFalse(correct_false)
+    def test_write(self):
+        target = Hdf5TableTarget(self.new, key="df")
+        with target.open("w") as tb:
+            tb.write(self.data)
+        df = target.open().read(autoclose=1)
+        pdt.assert_frame_equal(df, self.data)
 
-    def testTableRead(self):
-        target = Hdf5TableTarget(self.hdf5_file, "/df")
-        dfa = target.open()
-        pdt.assert_frame_equal(dfa, self.df)
+    def test_atomicity(self):
+        target = Hdf5TableTarget(self.new, key="df")
+        try:
+            with target.open("w") as tb:
+                tb.write(self.data.iloc[:100])
+                raise ValueError()
+        except ValueError:
+            self.assertFalse(target.exists())
 
-    def testRowRead(self):
-        target = Hdf5RowTarget(self.hdf5_file, "/df", row_expr=pd.Term("date > 20130101 & date < 20140101"))
-        dfa = target.open()
-        pdt.assert_frame_equal(dfa,
-                               self.df[(self.df.date > datetime(2013, 1, 1)) & (self.df.date < datetime(2014, 1, 1))])
+    def test_write_key_existent(self):
+        def temp_func():
+            target = Hdf5TableTarget(self.existing, key="df")
+            with target.open("w") as tb:
+                tb.write(self.data)
 
-    def testWrite(self):
-        target = Hdf5TableTarget(self.hdf5_file_empty, "/df")
-        target.write(self.df)
-        dfa = target.open()
-        pdt.assert_frame_equal(dfa, self.df)
-
-    def testDateExpression(self):
-        expr = "date >= {:%Y%m%d} & date <= {:%Y%m%d}".format(*(datetime(2013, 1, 1), datetime(2013, 5, 1)))
-        target = Hdf5RowTarget(self.hdf5_file, key="/df", row_expr=expr)
-        dfa = target.open()
-        df = self.df.query(expr)
-        self.assertTrue(target.exists, "Did not evaluate expression correctly:\n{}".format(expr))
-        pdt.assert_frame_equal(dfa, df)
-
-    def testSelectUserSubset(self):
-        expr = "id = [{}]".format(",".join(np.arange(100).astype(str)))
-        target = Hdf5RowTarget(self.hdf5_file, key="/df")
-        dfa = target.open(where=pd.Term(expr))
-        df = self.df[self.df.id.isin(np.arange(100))]
-        self.assertTrue(target.exists, "Did not evaluate expression correctly:\n{}".format(expr))
-        pdt.assert_frame_equal(dfa, df)
-
-    def testWriteMultiIndex(self):
-        target = Hdf5RowTarget(self.hdf5_file_empty, "/df", index_cols=("id", "date"))
-        target.write(self.df)
-        dfa = target.open(where=pd.Term("id > 0 & date >= 20130101"))
-        df = self.df[(self.df.id > 0) & (self.df.date >= "20130101")]
-        pdt.assert_frame_equal(dfa, df)
-
-    def testWriteSingleIndex(self):
-        target = Hdf5RowTarget(self.hdf5_file_empty, "/df", index_cols=("id",))
-        target.write(self.df)
-        dfa = target.open(where=pd.Term("id > 0"))
-        df = self.df[self.df.id > 0]
-        dfa.sort_values("id", inplace=True)
-        df.sort_values("id", inplace=True)
-        # print(set(dfa.id.unique()), set(df.id.unique()) )
-        pdt.assert_frame_equal(dfa, df)
-
-    def testAppend(self):
-        target = Hdf5TableTarget(self.hdf5_file_empty, "/df")
-        target.write(self.df.iloc[:100])
-        target.write(self.df.iloc[100:])
-        dfa = target.open()
-        pdt.assert_frame_equal(dfa, self.df)
-
-    def testChunkRead(self):
-        parts = []
-        target = Hdf5TableTarget(self.hdf5_file, "/df")
-        for df in target.read_chunks(1000):
-            parts.append(df)
-        dfa = pd.concat(parts)
-        pdt.assert_frame_equal(dfa, self.df)
-
-    def testChunkedExprReading(self):
-        parts = []
-        target = Hdf5TableTarget(self.hdf5_file, "/df")
-        for df in target.read_chunks(expr_list=["id = {}".format(i) for i in range(10)]):
-            parts.append(df)
-        dfa = pd.concat(parts)
-        dfa.sort_values(["id", "date"], inplace=True)
-        df = self.df.sort_values(["id", "date"])
-        pdt.assert_frame_equal(dfa, df)
-
-    def testMixedTypeColumn(self):
-        target = Hdf5TableTarget(self.hdf5_file_empty, "/df")
-        self.df.loc[0, "id"] = True
-        self.df.loc[1, "id"] = "somestring"
-        with self.assertRaises(TypeError):
-            target.write(self.df.iloc[:100])
+        self.assertRaises(ValueError, temp_func)
 
     def tearDown(self):
-        shutil.rmtree(self.dir)
+        if os.path.exists(self.existing):
+            os.remove(self.existing)
+        if os.path.exists(self.new):
+            os.remove(self.new)
+
+
+@attr('hdf5')
+class Hdf5TableTest(unittest.TestCase):
+    def setUp(self):
+        # create empty hdf5 file
+        self.store = tempfile.mktemp()
+
+        # create some data
+        n = 10000
+        self.nrows = n
+        lvl1 = np.random.choice(["some", "really", "random", "stuff", "not", "very", "creative"], size=n)
+        lvl2 = np.random.choice(["well", "some", "more", "levels", "could", "be", "nice"], size=n)
+        pids = list(map(lambda x: ".".join(x), zip(lvl1, lvl2)))
+        df = pd.DataFrame(dict(date=pd.date_range('20130101', periods=10000, freq="H"),
+                               page_id=pids,
+                               id=np.random.choice(np.arange(10), size=10000)))
+        self.data = pd.get_dummies(df, columns=["page_id"], prefix="", prefix_sep="")
+
+    @unittest.skipIf(not hasattr(unittest.TestCase, 'subTest'),
+                    'unittest.TestCase.subTest not present.')
+    def test_single_io(self):
+        with self.subTest("simple write"):
+            with AtomicHdf5Table(self.store, key="/df", mode="w", index_cols=("id",)) as table:
+                table.write(self.data)
+
+        with self.subTest("simple read"):
+            with AtomicHdf5Table(self.store, key="/df", mode="r", index_cols=("id",)) as table:
+                df = table.read()
+        pdt.assert_frame_equal(df, self.data.sort_index(axis=1))
+
+        with self.subTest("test_columns_sorted"):
+            self.assertTrue(cols_sorted(df))
+
+    @unittest.skipIf(not hasattr(unittest.TestCase, 'subTest'),
+                    'unittest.TestCase.subTest not present.')
+    def test_single_chunked_io(self):
+        with self.subTest("appending write"):
+            with AtomicHdf5Table(self.store, key="/df", mode="a", index_cols=("id",)) as table:
+                for i in range(0, self.data.shape[0], 1000):
+                    chunk = self.data.iloc[i:i + 1000]
+                    table.write(chunk)
+                df = table.read()
+            pdt.assert_frame_equal(df, self.data.sort_index(axis=1))
+
+        with self.subTest("chunked read"):
+            with AtomicHdf5Table(self.store, key="/df", mode="r", index_cols=("id",)) as table:
+                frames = []
+                for chunk in table.read(chunksize=1000):
+                    frames.append(chunk)
+                    self.assertTrue(cols_sorted(chunk))
+                df = pd.concat(frames, axis=0)
+            pdt.assert_frame_equal(df, self.data.sort_index(axis=1))
+
+        with self.subTest("read with start, stop"):
+            with AtomicHdf5Table(self.store, key="/df", mode="r", index_cols=("id",)) as table:
+                df = table.read(start=0, stop=1)
+            self.assertEqual(df.shape[0], 1)
+            self.assertTrue(cols_sorted(df))
+
+    @unittest.skipIf(not hasattr(unittest.TestCase, 'subTest'),
+                    'unittest.TestCase.subTest not present.')
+    def test_splitted_io(self):
+        with self.subTest("simple write"):
+            with AtomicHdf5Table(self.store, key="/df", mode="w", index_cols=("id",), split=4) as table:
+                table.write(self.data)
+
+            with AtomicHdf5Table(self.store, key="/df", mode="r", index_cols=("id",), split=4) as table:
+                self.assertTrue("/df/index_table" in table.fs)
+                self.assertTrue("/df/subtable_1" in table.fs)
+                self.assertTrue("/df/subtable_2" in table.fs)
+                self.assertTrue("/df/subtable_3" in table.fs)
+
+        with self.subTest("simple read"):
+            with AtomicHdf5Table(self.store, key="/df", mode="r", index_cols=("id",), split=4) as table:
+                df = table.read()
+            pdt.assert_frame_equal(df, self.data.sort_index(axis=1))
+
+        with self.subTest("read with start, stop"):
+            with AtomicHdf5Table(self.store, key="/df", mode="r", index_cols=("id",), split=4) as table:
+                df = table.read(start=0, stop=1)
+            self.assertEqual(df.shape[0], 1)
+            self.assertTrue(cols_sorted(df))
+
+    @unittest.skipIf(not hasattr(unittest.TestCase, 'subTest'),
+                    'unittest.TestCase.subTest not present.')
+    def test_manual_writing_splitted(self):
+        data_splits = [self.data[c_split] for c_split in np.array_split(self.data.columns, 4)]
+
+        with self.subTest("write table with sub_key"):
+            with AtomicHdf5Table(self.store, key="/df", mode="a") as table:
+                for i, char in enumerate(list("WHAT")):
+                    table.write(data_splits[i], sub_key="{}_table".format(char))
+
+        with self.subTest("full read"):
+            with AtomicHdf5Table(self.store, key="/df", mode="r") as table:
+                df = table.read()
+            pdt.assert_frame_equal(df, self.data.sort_index(axis=1))
+
+        with self.subTest("chunked read"):
+            with AtomicHdf5Table(self.store, key="/df", mode="r") as table:
+                frames = []
+                for chunk in table.read(chunksize=1000):
+                    frames.append(chunk)
+                #                    self.assertTrue(cols_sorted(chunk))
+                df = pd.concat(frames, axis=0)
+            pdt.assert_frame_equal(df, self.data.sort_index(axis=1))
+
+    @unittest.skipIf(not hasattr(unittest.TestCase, 'subTest'),
+                    'unittest.TestCase.subTest not present.')
+    def test_splitted_chunked_io(self):
+        with self.subTest("appending write"):
+            with AtomicHdf5Table(self.store, key="/df", mode="a", index_cols=("id",), split=4) as table:
+                for i in range(0, self.data.shape[0], 1000):
+                    chunk = self.data.iloc[i:i + 1000]
+                    table.write(chunk)
+
+            with AtomicHdf5Table(self.store, key="/df", mode="r") as table:
+                df = table.read()
+            pdt.assert_frame_equal(df, self.data.sort_index(axis=1))
+
+        with self.subTest("chunked read"):
+            with AtomicHdf5Table(self.store, key="/df", mode="r") as table:
+                frames = []
+                for chunk in table.read(chunksize=1000):
+                    frames.append(chunk)
+                    self.assertTrue(cols_sorted(chunk))
+                df = pd.concat(frames, axis=0)
+            pdt.assert_frame_equal(df, self.data.sort_index(axis=1))
+
+    @unittest.skipIf(not hasattr(unittest.TestCase, 'subTest'),
+                    'unittest.TestCase.subTest not present.')
+    def test_multi_key_io(self):
+        keys = ["table_{}".format(i) for i in range(10)]
+        with self.subTest("write multiple tables in same storage"):
+            for idx, key in enumerate(keys):
+                chunk = self.data.iloc[idx * 1000:(idx + 1) * 1000]
+                with AtomicHdf5Table(self.store, key=key, mode="a", index_cols=("id",)) as table:
+                    table.write(chunk)
+        with self.subTest("simple read"):
+            with AtomicHdf5Table(self.store, key=None, mode="r", index_cols=("id",)) as table:
+                df = table.read(keys=keys)
+            pdt.assert_frame_equal(df, self.data.sort_index(axis=1))
+        with self.subTest("read with select expr"):
+            with AtomicHdf5Table(self.store, key=None, mode="r", index_cols=("id",)) as table:
+                df = table.read(keys=keys, where="id >= 0 & id <=5")
+            sorted_df = self.data.sort_index(axis=1)
+            pdt.assert_frame_equal(df.reset_index(drop=1),
+                                   sorted_df[(sorted_df.id >= 0) & (sorted_df.id <= 5)].reset_index(drop=1))
+
+    @unittest.skipIf(not hasattr(unittest.TestCase, 'subTest'),
+                    'unittest.TestCase.subTest not present.')
+    def test_attributes(self):
+
+        with self.subTest("nrows"):
+            with AtomicHdf5Table(self.store, key="/df", mode="w", index_cols=("id",)) as table:
+                table.write(self.data)
+                self.assertEqual(table.nrows, self.nrows)
+        with self.subTest("fn"):
+            with AtomicHdf5Table(self.store, key="/df", mode="r", index_cols=("id",)) as table:
+                self.assertEqual(table.fn, self.store)
+
+    def test_forbidden_params(self):
+        pass
+
+    def test_atomicity(self):
+        try:
+            with AtomicHdf5Table(self.store, key="/df", mode="a", index_cols=("id",)) as table:
+                for i in range(0, self.data.shape[0], 1000):
+                    chunk = self.data.iloc[i:i + 1000]
+                    table.write(chunk)
+                    if i == 4 * 1000:
+                        raise ValueError()
+        except ValueError:
+            with AtomicHdf5Table(self.store, key=None, mode="r", index_cols=("id",)) as table:
+                self.assertFalse("/df" in table.fs)
+
+    def tearDown(self):
+        if os.path.exists(self.store):
+            os.remove(self.store)
