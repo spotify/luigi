2682b63de4e69fca4d26cb402ddf20cba0403ddd
Dave Buchfuhrer <dbuchfuhrer@gmail.com>
2016-06-26 22:20:20 -0700

    Removes unused import

--- luigi/rpc.py
+++ luigi/rpc.py
@@ -29,7 +29,7 @@ from luigi.six.moves.urllib.request import urlopen
 from luigi.six.moves.urllib.error import URLError
 
 from luigi import configuration
-from luigi.scheduler import PENDING, Scheduler, RPC_METHODS
+from luigi.scheduler import Scheduler, RPC_METHODS
 
 HAS_UNIX_SOCKET = True
 HAS_REQUESTS = True
