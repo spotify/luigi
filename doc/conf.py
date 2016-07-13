8eff203b304cf384d5fccb30bee37f15e6b3b0bf
Arash Rouhani <arashrk@vng.com.vn>
2016-07-13 10:28:38 +0700

    docs: Set minimum versions for sphinx
    I saw that the build was failing, hence this is my attempt to fix it,
    altough I don't know how to test this before merging it.
    See https://readthedocs.org/projects/luigi/builds/4189801/
    
    Unfortunately I couldn't find a way to connect a webhook between GitHub
    PRs and readthedocs: https://github.com/rtfd/readthedocs.org/issues/1340
    
    The reason I believe a version bump would fix it is because I see
    exactly the same error as in sphinx-doc/sphinx#2465 and readthedocs
    seem to use 1.3.5, causing the pdf docs to be outdated.

--- doc/conf.py
+++ doc/conf.py
@@ -82,7 +82,7 @@ autoclass_content = 'both'
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.4.4'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
