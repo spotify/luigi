06c69d06e763239f40770ef8a1551308506cffd7
Dave Buchfuhrer <buck@houzz.com>
2016-07-27 10:56:14 -0700

    Removes comment from NOQA in util.py

--- luigi/util.py
+++ luigi/util.py
@@ -224,7 +224,7 @@ from luigi import six
 
 from luigi import task
 from luigi import parameter
-from luigi.deprecate_kwarg import deprecate_kwarg  # NOQA: removing this breaks code
+from luigi.deprecate_kwarg import deprecate_kwarg  # NOQA
 
 if six.PY3:
     xrange = range
