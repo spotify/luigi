8e9906699dabc10395a0a31c1a0f8f1c77746d45
Arash Rouhani <miffoljud@gmail.com>
2016-08-02 17:24:19 +0700

    Revert "Added InputFileParameter and OutputFileParameter classes."

--- luigi/parameter.py
+++ luigi/parameter.py
@@ -29,7 +29,6 @@ from collections import OrderedDict, Mapping
 import operator
 import functools
 from ast import literal_eval
-import os
 
 try:
     from ConfigParser import NoOptionError, NoSectionError
@@ -960,38 +959,3 @@ class TupleParameter(Parameter):
         :param x: the value to serialize.
         """
         return json.dumps(x)
-
-
-class InputFileParameter(Parameter):
-    """
-    Paramater whose value is an existing file.
-    """
-
-    def parse(self, s):
-        """
-        Returns the real path to the file from the string if it exists.
-        """
-        if os.path.isfile(s):
-            return os.path.realpath(s)
-        else:
-            raise OSError("{s} does not exist".format(s=s))
-
-
-class OutputFileParameter(Parameter):
-    """
-    Parameter whose value is an output file to be created.
-    """
-
-    def parse(self, s):
-        """
-        Returns the real path to the file and attempts to create any directories
-        needed.
-        """
-        s = os.path.realpath(s)
-        if not os.path.isdir(os.path.dirname(s)):
-            try:
-                os.makedirs(os.path.dirname(s))
-            except OSError:
-                raise OSError("could not make parent directories for {s}".format(
-                    s=s))
-        return s
