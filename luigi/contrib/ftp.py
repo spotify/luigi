396d138d5b3183f94dd322076662a35f44d69da6
Peter Gaultney <petergaultney@gmail.com>
2016-08-11 14:47:31 -0500

    Added passthrough for PySFTP Connection keyword arguments to RemoteTarget constructor (#1815)
    * Added passthrough for PySFTP Connection keyword arguments.
    
    There's no reason Luigi shouldn't support things like SFTP
    using a private key file, for instance.
    
    This doesn't alter the interface for existing users - it simply
    allows users who know that PySFTP is the underlying implementation
    to pass additional keyword arguments to the creation of the PySFTP
    Connection.
    
    * added pysftp kwargs to RemoteTarget

--- luigi/contrib/ftp.py
+++ luigi/contrib/ftp.py
@@ -45,13 +45,15 @@ logger = logging.getLogger('luigi-interface')
 
 class RemoteFileSystem(luigi.target.FileSystem):
 
-    def __init__(self, host, username=None, password=None, port=None, tls=False, timeout=60, sftp=False):
+    def __init__(self, host, username=None, password=None, port=None,
+                 tls=False, timeout=60, sftp=False, pysftp_conn_kwargs=None):
         self.host = host
         self.username = username
         self.password = password
         self.tls = tls
         self.timeout = timeout
         self.sftp = sftp
+        self.pysftp_conn_kwargs = pysftp_conn_kwargs
 
         if port is None:
             if self.sftp:
@@ -76,7 +78,8 @@ class RemoteFileSystem(luigi.target.FileSystem):
         except ImportError:
             logger.warning('Please install pysftp to use SFTP.')
 
-        self.conn = pysftp.Connection(self.host, username=self.username, password=self.password, port=self.port)
+        self.conn = pysftp.Connection(self.host, username=self.username, password=self.password,
+                                      port=self.port, **self.pysftp_conn_kwargs)
 
     def _ftp_connect(self):
         if self.tls:
@@ -350,7 +353,7 @@ class RemoteTarget(luigi.target.FileSystemTarget):
     def __init__(
         self, path, host, format=None, username=None,
         password=None, port=None, mtime=None, tls=False,
-        timeout=60, sftp=False
+            timeout=60, sftp=False, pysftp_conn_kwargs=None
     ):
         if format is None:
             format = luigi.format.get_default_format()
@@ -361,7 +364,7 @@ class RemoteTarget(luigi.target.FileSystemTarget):
         self.tls = tls
         self.timeout = timeout
         self.sftp = sftp
-        self._fs = RemoteFileSystem(host, username, password, port, tls, timeout, sftp)
+        self._fs = RemoteFileSystem(host, username, password, port, tls, timeout, sftp, pysftp_conn_kwargs)
 
     @property
     def fs(self):
