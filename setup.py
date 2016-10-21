35b340d7da62613c645ae37b6feb00147c7a9e52
Arash Rouhani <arashrk@vng.com.vn>
2016-10-21 11:03:20 +0700

    Version 2.3.3

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ if os.environ.get('READTHEDOCS', None) == 'True':
 
 setup(
     name='luigi',
-    version='2.3.2',
+    version='2.3.3',
     description='Workflow mgmgt + task scheduling + dependency resolution',
     long_description=long_description,
     author='Erik Bernhardsson',
