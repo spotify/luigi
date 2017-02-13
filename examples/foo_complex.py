73e782dbaefa867d37b3752ce14aeca84221ddc4
Álvaro Marco <amarco90@gmail.com>
2017-02-13 19:50:02 +0100

    Introduce Python3 compatibility in example

--- examples/foo_complex.py
+++ examples/foo_complex.py
@@ -23,6 +23,7 @@ You can run this example like this:
             $ luigi --module examples.foo_complex examples.Foo --workers 2 --local-scheduler
 
 """
+from __future__ import division
 import time
 import random
 
@@ -41,7 +42,7 @@ class Foo(luigi.Task):
 
     def requires(self):
         global current_nodes
-        for i in range(30 / max_depth):
+        for i in range(30 // max_depth):
             current_nodes += 1
             yield Bar(i)
 
@@ -60,7 +61,7 @@ class Bar(luigi.Task):
 
         if max_total_nodes > current_nodes:
             valor = int(random.uniform(1, 30))
-            for i in range(valor / max_depth):
+            for i in range(valor // max_depth):
                 current_nodes += 1
                 yield Bar(current_nodes)
 
