37c3b2d091cfc0ff28db78d02ea5451ca7fd81dd
colemanja91 <colemanja91@gmail.com>
2017-02-19 08:33:58 -0500

    instead of trying to dynamically generate open format, use wb

--- luigi/contrib/salesforce.py
+++ luigi/contrib/salesforce.py
@@ -183,16 +183,14 @@ class QuerySalesforce(Task):
                 # If there's only one result, just download it, otherwise we need to merge the resulting downloads
                 if len(result_ids) == 1:
                     data = sf.get_batch_result(job_id, batch_id, result_ids[0])
-                    with open(self.output().path,
-                              luigi.format.get_default_format()) as outfile:
+                    with open(self.output().path, 'wb') as outfile:
                         outfile.write(data)
                 else:
                     # Download each file to disk, and then merge into one.
                     # Preferring to do it this way so as to minimize memory consumption.
                     for i, result_id in enumerate(result_ids):
                         logger.info("Downloading batch result %s for batch: %s and job: %s" % (result_id, batch_id, job_id))
-                        with open("%s.%d" % (self.output().path, i),
-                                  luigi.format.get_default_format()) as outfile:
+                        with open("%s.%d" % (self.output().path, i), 'wb') as outfile:
                             outfile.write(sf.get_batch_result(job_id, batch_id, result_id))
 
                     logger.info("Merging results of batch %s" % batch_id)
@@ -206,8 +204,7 @@ class QuerySalesforce(Task):
             data_file = sf.query_all(self.soql)
 
             reader = csv.reader(data_file)
-            with open(self.output().path,
-                      luigi.format.get_default_format()) as outfile:
+            with open(self.output().path, 'wb') as outfile:
                 writer = csv.writer(outfile, dialect='excel')
                 for row in reader:
                     writer.writerow(row)
@@ -216,8 +213,7 @@ class QuerySalesforce(Task):
         """
         Merges the resulting files of a multi-result batch bulk query.
         """
-        outfile = open(self.output().path,
-                       luigi.format.get_default_format())
+        outfile = open(self.output().path, 'wb')
 
         if self.content_type.lower() == 'csv':
             for i, result_id in enumerate(result_ids):
