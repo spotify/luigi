a41e4c4384845c21f5854cf697b3f83535dedb5f
Antoine Jouanjean <antoine.jouanjean@gmail.com>
2017-03-14 22:05:07 +0100

    Add 'IF NOT EXISTS' to prevent duplicate table exception
    PR for issue #2060: Avoid exception handling for PostgresTarget.create_marker_table() with "CREATE TABLE IF NOT EXISTS"

--- luigi/contrib/postgres.py
+++ luigi/contrib/postgres.py
@@ -207,13 +207,13 @@ class PostgresTarget(luigi.Target):
         connection.autocommit = True
         cursor = connection.cursor()
         if self.use_db_timestamps:
-            sql = """ CREATE TABLE {marker_table} (
+            sql = """ CREATE TABLE IF NOT EXISTS {marker_table} (
                       update_id TEXT PRIMARY KEY,
                       target_table TEXT,
                       inserted TIMESTAMP DEFAULT NOW())
-                """.format(marker_table=self.marker_table)
+                  """.format(marker_table=self.marker_table)
         else:
-            sql = """ CREATE TABLE {marker_table} (
+            sql = """ CREATE TABLE IF NOT EXISTS {marker_table} (
                       update_id TEXT PRIMARY KEY,
                       target_table TEXT,
                       inserted TIMESTAMP);
@@ -221,10 +221,7 @@ class PostgresTarget(luigi.Target):
         try:
             cursor.execute(sql)
         except psycopg2.ProgrammingError as e:
-            if e.pgcode == psycopg2.errorcodes.DUPLICATE_TABLE:
-                pass
-            else:
-                raise
+            raise
         connection.close()
 
     def open(self, mode):
