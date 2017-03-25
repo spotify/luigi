08087a30ec70e3ac07efadc6f5000eaf1d058cd5
Nelson Onyibe <nelsononyibe@Nelson-Onyibes-MacBook-Pro.local>
2017-03-24 20:46:13 -0400

    using luigi 1.2.2 without pyparsing

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ if os.environ.get('READTHEDOCS', None) == 'True':
 
 setup(
     name='luigi',
-    version='1.3.0',
+    version='1.2.2',
     description='Workflow mgmgt + task scheduling + dependency resolution',
     long_description=long_description,
     author='Erik Bernhardsson',
