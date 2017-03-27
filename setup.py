ac2840823fdfdbae2fd182bb0bfe0fe037e3208c
Nelson Onyibe <nelsononyibe@Nelson-Onyibes-MacBook-Pro.local>
2017-03-27 14:35:47 -0400

    Attempting luigi 2.0

--- setup.py
+++ setup.py
@@ -42,19 +42,19 @@ with open('README.rst') as fobj:
 install_requires = [
     'cached_property',
     'tornado',
-    'python-daemon==2.1.2',
+    'python-daemon<3.0',
     'retrying'
 ]
 
 if os.environ.get('READTHEDOCS', None) == 'True':
     install_requires.append('sqlalchemy')
     # readthedocs don't like python-daemon, see #1342
-    install_requires.remove('python-daemon==2.1.2')
+    install_requires.remove('python-daemon<3.0')
     # So that we can build documentation for luigi.db_task_history and luigi.contrib.sqla
 
 setup(
     name='luigi',
-    version='1.2.2',
+    version='2.0.0',
     description='Workflow mgmgt + task scheduling + dependency resolution',
     long_description=long_description,
     author='Erik Bernhardsson',
