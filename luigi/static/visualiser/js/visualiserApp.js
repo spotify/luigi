b73d2f284ae326921c6df3f3b2120c0aee6c4f03
Alex Brausewetter <alex@xoob.de>
2017-04-17 16:46:37 +0200

    Fix "Show Upstream Dependencies" in visualiser
    Fixes the "Show Upstream Dependencies" checkbox which did not have any effect

--- luigi/static/visualiser/js/visualiserApp.js
+++ luigi/static/visualiser/js/visualiserApp.js
@@ -413,7 +413,7 @@ function visualiserApp(luigi) {
             if (taskId) {
                 var depGraphCallback = makeGraphCallback(fragmentQuery.visType, taskId, paint);
 
-                if (fragmentQuery.invertDependencies) {
+                if (fragmentQuery.invert) {
                     luigi.getInverseDependencyGraph(taskId, depGraphCallback, !hideDone);
                 } else {
                     luigi.getDependencyGraph(taskId, depGraphCallback, !hideDone);
