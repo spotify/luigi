20ba9a9c658a1c5333770a68ccda9a561225dcdc
Fabien Vinas <fabien@ehda.co>
2017-07-04 22:44:06 +0200

    Unit test

--- test/task_progress_percentage_test.py
+++ test/task_progress_percentage_test.py
@@ -21,21 +21,18 @@ import luigi
 import luigi.scheduler
 import luigi.worker
 
-luigi.notifications.DEBUG = True
-
 
 class TaskProgressPercentageTest(LuigiTestCase):
 
     def test_run(self):
-        percentage = 30
         sch = luigi.scheduler.Scheduler()
         with luigi.worker.Worker(scheduler=sch) as w:
             class MyTask(luigi.Task):
                 def run(self):
-                    self.set_progress_percentage(percentage)
+                    self.set_progress_percentage(30)
 
             task = MyTask()
             w.add(task)
             w.run()
 
-            self.assertEqual(sch.get_task_progress_percentage(task.task_id)["progressPercentage"], percentage)
+            self.assertEqual(sch.get_task_progress_percentage(task.task_id)["progressPercentage"], 30)
