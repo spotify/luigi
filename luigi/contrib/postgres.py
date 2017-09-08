65a6293614565875d5aad55bb0f6a5fbcae26ff6
Angeline <angelinemm@gmail.com>
2017-09-08 13:24:09 +0100

    Fix "IF NOT EXISTS" Postgres 8 incompatibility (#2228)
    Reverts PR #2064 to allow Postgres 8 compatibility. Companies may (and do) use Postgres 8 as a dev environment for their production Redshift cluster (as Redshift if based on Postgres 8.0.2).

--- luigi/contrib/postgres.py
+++ luigi/contrib/postgres.py
@@ -204,19 +204,25 @@ class PostgresTarget(luigi.Target):
         connection.autocommit = True
         cursor = connection.cursor()
         if self.use_db_timestamps:
-            sql = """ CREATE TABLE IF NOT EXISTS {marker_table} (
+            sql = """ CREATE TABLE {marker_table} (
                       update_id TEXT PRIMARY KEY,
                       target_table TEXT,
                       inserted TIMESTAMP DEFAULT NOW())
                   """.format(marker_table=self.marker_table)
         else:
-            sql = """ CREATE TABLE IF NOT EXISTS {marker_table} (
+            sql = """ CREATE TABLE {marker_table} (
                       update_id TEXT PRIMARY KEY,
                       target_table TEXT,
                       inserted TIMESTAMP);
                   """.format(marker_table=self.marker_table)
 
-        cursor.execute(sql)
+        try:
+            cursor.execute(sql)
+        except psycopg2.ProgrammingError as e:
+            if e.pgcode == psycopg2.errorcodes.DUPLICATE_TABLE:
+                pass
+            else:
+                raise
         connection.close()
 
     def open(self, mode):
