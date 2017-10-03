9cbf18822c74790009841c3305c0ae9d77726726
James McMurray <jamesmcm03@gmail.com>
2017-10-03 15:06:41 +0200

    Use columns defined for COPY to Redshift so we never rely on order (#2245)
    * Use columns defined for COPY to Redshift so we never rely on order

--- luigi/contrib/redshift.py
+++ luigi/contrib/redshift.py
@@ -334,12 +334,18 @@ class S3CopyToTable(rdbms.CopyToTable, _CredentialsMixin):
         If both key-based and role-based credentials are provided, role-based will be used.
         """
         logger.info("Inserting file: %s", f)
+        colnames = ''
+        if len(self.columns) > 0:
+            colnames = ",".join([x[0] for x in self.columns])
+            colnames = '({})'.format(colnames)
+
         cursor.execute("""
-         COPY {table} from '{source}'
+         COPY {table} {colnames} from '{source}'
          CREDENTIALS '{creds}'
          {options}
          ;""".format(
             table=self.table,
+            colnames=colnames,
             source=f,
             creds=self._credentials(),
             options=self.copy_options)
