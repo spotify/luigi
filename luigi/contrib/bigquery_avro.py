7720035e16c0b375ef812761539776a94b8de62d
Idrees Khan <idrees@spotify.com>
2017-10-25 14:23:25 -0400

    Cleanup irrelevant comment, remove unused imports

--- luigi/contrib/bigquery_avro.py
+++ luigi/contrib/bigquery_avro.py
@@ -7,22 +7,9 @@ from luigi.task import flatten
 
 logger = logging.getLogger('luigi-interface')
 
-try:
-    import avro
-    import avro.datafile
-except ImportError:
-    logger.warning('bigquery_avro module imported, but avro is not installed. Any '
-                   'BigQueryLoadAvro task will fail to propagate schema documentation')
-
 
 class BigQueryLoadAvro(BigQueryLoadTask):
     """A helper for loading specifically Avro data into BigQuery from GCS.
-
-    Additional goodies - takes field documentation from the input data and propagates it
-    to BigQuery table description and field descriptions.  Supports the following Avro schema
-    types: Primitives, Enums, Records, Arrays, Unions, and Maps.  For Map schemas nested maps
-    and unions are not supported.  For Union Schemas only nested Primitive and Record Schemas
-    are currently supported.
 
     Suitable for use via subclassing: override requires() to return Task(s) that output
     to GCS Targets; their paths are expected to be URIs of .avro files or URI prefixes
