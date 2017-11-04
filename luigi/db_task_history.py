56fbbcb9be4c97a88c25fa527881f5bbade9cd41
Dillon Stadther <dlstadther+github@gmail.com>
2017-11-04 14:27:40 -0400

    Fix new flake8 tests (#2274)
    * Ignore new flake8 tests
    
    These new exclusions include:
    * E722 - bare except
    * E741 - ambiguous variable name
    * E742 - ambiguous class definition
    
    * Update flake8 calls for corrected exclusions
    
    * Revert flake8 test exclusions
    
    * Fix instance of E742 violation
    
    * Fix instance of E741 violation
    
    * Replace bare except with BaseException
    
    * Replace bare except with AttributeError
    
    * Remove unnecessary try/except

--- luigi/db_task_history.py
+++ luigi/db_task_history.py
@@ -70,7 +70,7 @@ class DbTaskHistory(task_history.TaskHistory):
             session = self.session_factory()
             try:
                 yield session
-            except:
+            except BaseException:
                 session.rollback()
                 raise
             else:
