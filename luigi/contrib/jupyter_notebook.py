80ef30d0eefc6d356d4d3d7255d3b915dd8e7a8d
mattiaciollaro <mattiaciollaro@gmail.com>
2017-11-07 15:01:48 -0500

    Address review suggestions

--- luigi/contrib/jupyter_notebook.py
+++ luigi/contrib/jupyter_notebook.py
@@ -20,15 +20,15 @@ task within a Luigi pipeline. This can be accomplished via the
 :class:`JupyterNotebookTask` class.
 
 :class:`JupyterNotebookTask` allows you to pass parameters to the Jupyter
-notebook through the ``pars`` dictionary.
+notebook through the ``parameters`` dictionary.
 
-When the task is executed, the ``pars`` dictionary is written to a
+When the task is executed, the ``parameters`` dictionary is written to a
 temporary JSON file in the same directory that contains the notebook.
 
 Inside the notebook, you can retrieve the values of the parameters in
-``pars`` by reading the temporary JSON file. The path to the temporary file is
-accessible via the environment variable ``PARS``.
-For example, in a Python notebook, you can read the ``pars`` dictionary as
+``parameters`` by reading the temporary JSON file. The path to the temporary
+file is accessible via the environment variable ``PARS``.
+For example, in a Python notebook, you can read the ``parameters`` dictionary as
 follows:
 
 .. code-block:: python
@@ -49,7 +49,7 @@ follows:
     # extract a *user-defined* parameter named `my_par`
     my_par = pars.get('my_par')
 
-Similarly, in a R notebook, you can read the ``pars`` dictionary with
+Similarly, in a R notebook, you can read the ``parameters`` dictionary with
 
 .. code-block:: r
 
@@ -68,7 +68,7 @@ Similarly, in a R notebook, you can read the ``pars`` dictionary with
     my_par <- pars$my_par
 
 The paths of the task's ``self.input()`` and
-``self.output()`` are automatically added to ``pars`` with keys
+``self.output()`` are automatically added to ``parameters`` with keys
 *input* and *output* respectively. These paths are meaningful when
 ``self.input()`` and ``self.output()`` return iterables or dictionaries whose
 values have a ``path`` attribute
@@ -106,6 +106,7 @@ Written by `@mattiaciollaro <https://github.com/mattiaciollaro>`_.
 import json
 import logging
 import os
+from datetime import datetime
 
 import luigi
 from luigi.task import flatten
@@ -150,6 +151,7 @@ def _get_values(obj):
     """
     Creates a flat list of all all items in structured output
     (dicts, lists, items).
+    This is just a wrapper around the `luigi.task.flatten()` utility.
     """
     return flatten(obj)
 
@@ -171,7 +173,7 @@ class JupyterNotebookTask(luigi.Task):
     This is a template task to execute Jupyter notebooks as Luigi tasks.
     This task has the following key parameters:
 
-    :param nb_path: the full path to the Jupyter notebook (**required**).
+    :param notebook_path: the full path to the Jupyter notebook (**required**).
 
     :param kernel_name: the name of the kernel to be used in the notebook
         execution (**required**).
@@ -179,37 +181,52 @@ class JupyterNotebookTask(luigi.Task):
     :param timeout: maximum time (in seconds) allocated to run each cell.
         If -1 (the default), no timeout limit is imposed.
 
-    :param pars: a dictionary of user-defined parameters to be passed to the
-        Jupyter notebook.
+    :param parameters: a dictionary of user-defined parameters to be passed to
+        the Jupyter notebook.
 
     :param json_action: if `delete` (default), the temporary JSON file with
-        **pars** is deleted at the end of the task execution; if `keep`, the
-        temporary JSON file is kept (useful for debugging purposes).
+        **parameters** is deleted at the end of the task execution; if `keep`,
+        the temporary JSON file is kept (useful for debugging purposes).
     """
-    nb_path = luigi.Parameter()
+    notebook_path = luigi.Parameter(
+        description='The full path to the Jupyter notebook'
+    )
 
-    kernel_name = luigi.Parameter()
+    kernel_name = luigi.Parameter(
+        description='The Jupyter kernel to be used in the notebook execution'
+    )
 
     timeout = luigi.IntParameter(
+        description='Max time (in seconds) allocated to run each notebook cell',
         default=-1
     )
 
     json_action = luigi.ChoiceParameter(
+        description="'delete' or 'keep' the temporary JSON file",
         choices=['delete', 'keep'],
         var_type=str,
         default='delete'
     )
 
-    pars = {}
+    parameters = {}
 
     def _form_input(self):
-
+        """
+        This method is used to loop through the return value of the task's
+        `requires` method and extract the paths of the returned `LocalTarget`s.
+        For any item returned by `requires` that does not have a `path`
+        attribute, the corresponding extracted value is `None`.
+        """
+        # case 1 - `requires` returns a dictionary
+        # In this case, the output of `_form_input()` is a dictionary of lists.
         if isinstance(self.input(), dict):
             out = {
                 tag: list(map(
                     _get_path, _get_values(self.input().get(tag)))
                 ) for tag in self.input().keys()
             }
+        # case 2 - `requires` returns a list or other iterable
+        # In this case, the output of `_form_input()` is a list of lists.
         else:
             out = [
                 list(map(_get_path, _get_values(req)))
@@ -219,11 +236,20 @@ class JupyterNotebookTask(luigi.Task):
         return out
 
     def _form_output(self):
-
+        """
+        This method is used to loop through the return value of the task's
+        `output` method and extract the paths of the returned `LocalTarget`s.
+        For any item returned by `output` that does not have a `path`
+        attribute, the corresponding extracted value is `None`.
+        """
+        # case 1 - `output` returns a dictionary
+        # In this case, the output of `_form_output()` is a dictionary.
         if isinstance(self.output(), dict):
             out = {
                 tag: _get_path(req) for tag, req in self.output().items()
             }
+        # case 2 - `output` returns a list or other iterable
+        # In this case, the output of `_form_output()` is a list.
         else:
             out = [_get_path(req) for req in _get_values(self.output())]
 
@@ -231,40 +257,41 @@ class JupyterNotebookTask(luigi.Task):
 
     def run(self):
 
-        self.timeout = int(self.timeout)
+        # get current date and time
+        time = datetime.strftime(datetime.now(), '%Y-%m-%d_%H-%M-%S')
 
         # get notebook name
-        notebook_name = _get_file_name_from_path(self.nb_path)
+        notebook_name = _get_file_name_from_path(self.notebook_path)
 
         # get task id
         task_id = self.task_id
 
-        # set requires pars
-        self.pars['input'] = self._form_input()
+        # set requires parameters
+        self.parameters['input'] = self._form_input()
 
-        # set output pars
-        self.pars['output'] = self._form_output()
+        # set output parameters
+        self.parameters['output'] = self._form_output()
 
-        # write pars to temporary file
+        # write parameters to temporary file
         tmp_file_path = os.path.join(
-            os.path.dirname(self.nb_path), '%s_%s.ipynbpars'
-            % (notebook_name, task_id)
+            os.path.dirname(self.notebook_path), '%s_%s_%s.ipynbpars'
+            % (notebook_name, task_id, time)
         )
 
-        with open(tmp_file_path, 'w') as parameters:
-            json.dump(self.pars, parameters)
+        with open(tmp_file_path, 'w') as params:
+            json.dump(self.parameters, params)
 
         # set environment variable with tmp_file_path
         os.environ['PARS'] = tmp_file_path
 
         # run notebook
-        logger.info('=== Running notebook: %s ===' % notebook_name)
+        logger.info('===== Running notebook: %s =====' % notebook_name)
 
-        nb_version = int(nbformat.__version__.split('.')[0])
+        notebook_version = int(nbformat.__version__.split('.')[0])
 
         try:
-            with open(self.nb_path, 'r') as nb:
-                nb = nbformat.read(nb, as_version=nb_version)
+            with open(self.notebook_path, 'r') as nb:
+                nb = nbformat.read(nb, as_version=notebook_version)
 
             ep = ExecutePreprocessor(
                 timeout=self.timeout,
@@ -273,14 +300,16 @@ class JupyterNotebookTask(luigi.Task):
 
             ep.preprocess(nb, {
                 'metadata': {
-                    'path': os.path.dirname(self.nb_path)
+                    'path': os.path.dirname(self.notebook_path)
                 }
             })
 
-            logger.info('=== Done! ===')
+            logger.info('===== Done! =====')
 
         except CellExecutionError:
+            logger.info('===== Done with errors! =====')
             raise
+
 
         finally:
             # clean up (remove temporary JSON file)
