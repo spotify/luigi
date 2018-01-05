10599268532383d7ddd05fe82e3393d9178931b0
Ouanis Seddaoui <ouanis.seddaoui@skyscanner.net>
2018-01-05 10:06:01 +0000

    fixed as per PR's comments

--- luigi/contrib/s3.py
+++ luigi/contrib/s3.py
@@ -186,8 +186,7 @@ class S3Client(FileSystem):
         s3_bucket = self.s3.Bucket(bucket)
         # root
         if self._is_root(key):
-            raise InvalidDeleteException(
-                'Cannot delete root of bucket at path %s' % path)
+            raise InvalidDeleteException('Cannot delete root of bucket at path %s' % path)
 
         # file
         if self._exists(bucket, key):
@@ -196,20 +195,16 @@ class S3Client(FileSystem):
             return True
 
         if self.isdir(path) and not recursive:
-            raise InvalidDeleteException(
-                'Path %s is a directory. Must use recursive delete' % path)
+            raise InvalidDeleteException('Path %s is a directory. Must use recursive delete' % path)
 
-        delete_key_list = []
-        for obj in s3_bucket.objects.filter(Prefix=self._add_path_delimiter(key)):
-            delete_key_list.append({'Key': obj.key})
+        delete_key_list = [{'Key': obj.key} for obj in s3_bucket.objects.filter(Prefix=self._add_path_delimiter(key))]
 
         # delete the directory marker file if it exists
-        if self._exists(bucket, key + S3_DIRECTORY_MARKER_SUFFIX_0):
-            delete_key_list.append({'Key': key + S3_DIRECTORY_MARKER_SUFFIX_0})
+        if self._exists(bucket, '{}{}'.format(key, S3_DIRECTORY_MARKER_SUFFIX_0)):
+            delete_key_list.append({'Key': '{}{}'.format(key, S3_DIRECTORY_MARKER_SUFFIX_0)})
 
         if len(delete_key_list) > 0:
-            self.s3.meta.client.delete_objects(
-                Bucket=bucket, Delete={'Objects': delete_key_list})
+            self.s3.meta.client.delete_objects(Bucket=bucket, Delete={'Objects': delete_key_list})
             return True
 
         return False
@@ -475,8 +470,8 @@ class S3Client(FileSystem):
                 pass
         if key:
             return config.get(key)
-        section_only = {k: v for k, v in config.items(
-        ) if k not in defaults or v != defaults[k]}
+        section_only = {k: v for k, v in config.items() if k not in defaults or v != defaults[k]}
+        
         return section_only
 
     def _path_to_bucket_and_key(self, path):
