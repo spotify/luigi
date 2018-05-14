e4d4a07a925fb3a8ac020b7acae105c992fc9f6e
Dillon Stadther <dlstadther@gmail.com>
2018-05-14 14:26:21 -0400

    Reenables true multithreaded s3 copy
    Boto3 update removed multithreaded file copy in favor of multithreaded multipart file copy.

--- luigi/contrib/s3.py
+++ luigi/contrib/s3.py
@@ -30,6 +30,8 @@ import os
 import os.path
 import warnings
 
+from multiprocessing.pool import ThreadPool
+
 import botocore
 
 try:
@@ -323,6 +325,9 @@ class S3Client(FileSystem):
         total_keys = 0
 
         if self.isdir(source_path):
+            copy_jobs = []
+            management_pool = ThreadPool(processes=threads)
+
             (bucket, key) = self._path_to_bucket_and_key(source_path)
             key_path = self._add_path_delimiter(key)
             key_path_len = len(key_path)
@@ -340,8 +345,19 @@ class S3Client(FileSystem):
                         'Key': src_prefix + path
                     }
 
-                    self.s3.meta.client.copy(
-                        copy_source, dst_bucket, dst_prefix + path, Config=transfer_config, ExtraArgs=kwargs)
+                    the_kwargs = {'Config': transfer_config, 'ExtraArgs': kwargs}
+                    job = management_pool.apply_async(self.s3.meta.client.copy,
+                                                      args=(copy_source, dst_bucket, dst_prefix + path),
+                                                      kwds=the_kwargs)
+                    copy_jobs.append(job)
+
+            # Wait for the pools to finish scheduling all the copies
+            management_pool.close()
+            management_pool.join()
+
+            # Raise any errors encountered in any of the copy processes
+            for result in copy_jobs:
+                result.get()
 
             end = datetime.datetime.now()
             duration = end - start
