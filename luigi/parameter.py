c7cab1c86d1581550c5e565582618994f3550e24
Marcel R <marcelrieger@me.com>
2018-07-05 17:41:46 +0900

    Add class docs to BoolParameter.

--- luigi/parameter.py
+++ luigi/parameter.py
@@ -612,8 +612,31 @@ class FloatParameter(Parameter):
 
 class BoolParameter(Parameter):
     """
-    A Parameter whose value is a ``bool``. This parameter have an implicit
-    default value of ``False``.
+    A Parameter whose value is a ``bool``. This parameter has an implicit default value of
+    ``False``. For the command line interface this means that the value is ``False`` unless you
+    add ``"--the-bool-parameter"`` to your command without giving a parameter value. This is
+    considered *implicit* parsing (the default). However, in some situations one might want to give
+    the explicit bool value (``"--the-bool-parameter true|false"``), e.g. when you configure the
+    default value to be ``True``. This is called *explicit* parsing. When omitting the parameter
+    value, it is still considered ``True`` but to avoid ambiguities during argument parsing, make
+    sure to always place bool parameters behind the task family on the command line when using
+    explicit parsing.
+
+    You can toggle between the two parsing modes on a per-parameter base via
+
+    .. code-block:: python
+
+        class MyTask(luigi.Task):
+            implicit_bool = luigi.BoolParameter(parsing=luigi.BoolParameter.IMPLICIT_PARSING)
+            explicit_bool = luigi.BoolParameter(parsing=luigi.BoolParameter.EXPLICIT_PARSING)
+
+    or globally by
+
+    .. code-block:: python
+
+        luigi.BoolParameter.parsing = luigi.BoolParameter.EXPLICIT_PARSING
+
+    for all bool parameters instantiated after this line.
     """
 
     IMPLICIT_PARSING = "implicit"
