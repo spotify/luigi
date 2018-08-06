25809b27b03f801c3b41b5219d001f38a6555f53
Gram <master_fess@mail.ru>
2018-08-06 15:45:37 +0300

    +copyright

--- luigi/configuration/toml_parser.py
+++ luigi/configuration/toml_parser.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2018 Cindicator Ltd.
+# Copyright 2018 Vote Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
