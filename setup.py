c92311703b3ca573c05893aecf39f570366e37d8
Dillon Stadther <dlstadther@gmail.com>
2018-08-31 22:21:20 -0400

    Update package classifiers for supported Python versions

--- setup.py
+++ setup.py
@@ -94,6 +94,8 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: System :: Monitoring',
     ],
 )
