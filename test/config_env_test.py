0c5faca53daf7f578035e8b1d8289f74e6b7c42b
Piotr Janiszewski <i.am.like.me@gmail.com>
2018-09-23 12:38:55 +0200

    Correct the unit tests

--- test/config_env_test.py
+++ test/config_env_test.py
@@ -19,7 +19,7 @@ import os
 from luigi.configuration import LuigiConfigParser, get_config
 from luigi.configuration.cfg_parser import InterpolationMissingEnvvarError
 
-from helpers import LuigiTestCase
+from helpers import LuigiTestCase, with_config
 
 
 class ConfigParserTest(LuigiTestCase):
@@ -44,22 +44,26 @@ class ConfigParserTest(LuigiTestCase):
         for key, value in self.environ_backup:
             os.environ[key] = value
 
+    @with_config({"test": {
+        "a": "testval",
+        "b": "%(a)s",
+        "c": "%(a)s%(a)s",
+    }})
     def test_basic_interpolation(self):
         # Make sure the default ConfigParser behaviour is not broken
         config = get_config()
-        config.set("test", "a", "testval")
-        config.set("test", "b", "%(a)s")
-        config.set("test", "c", "%(a)s%(a)s")
 
         self.assertEqual(config.get("test", "b"), config.get("test", "a"))
         self.assertEqual(config.get("test", "c"), 2 * config.get("test", "a"))
 
+    @with_config({"test": {
+        "a": "${TESTVAR}",
+        "b": "${TESTVAR} ${TESTVAR}",
+        "c": "${TESTVAR} %(a)s",
+        "d": "${NONEXISTING}",
+    }})
     def test_env_interpolation(self):
         config = get_config()
-        config.set("test", "a", "${TESTVAR}")
-        config.set("test", "b", "${TESTVAR} ${TESTVAR}")
-        config.set("test", "c", "${TESTVAR} %(a)s")
-        config.set("test", "d", "${NONEXISTING}")
 
         self.assertEqual(config.get("test", "a"), "1")
         self.assertEqual(config.getint("test", "a"), 1)
