89f542548fff56ac759366288a7d8bb7098d95e1
Hichame El Khalfi <hichame@elkhalfi.com>
2018-10-09 15:43:21 -0400

    add fix to HdfsAtomicWritePipe

--- luigi/contrib/hdfs/format.py
+++ luigi/contrib/hdfs/format.py
@@ -51,8 +51,12 @@ class HdfsAtomicWritePipe(luigi.format.OutputPipeProcessWrapper):
         super(HdfsAtomicWritePipe, self).close()
         try:
             remove(self.path)
-        except HDFSCliError:
-            pass
+        except Exception, ex:
+            if isinstance(ex, HDFSCliError) or ex.args[0].contains("FileNotFoundException"):
+                pass
+            else:
+                raise ex
+
         if not all(result['result'] for result in rename(self.tmppath, self.path) or []):
             raise HdfsAtomicWriteError('Atomic write to {} failed'.format(self.path))
 
