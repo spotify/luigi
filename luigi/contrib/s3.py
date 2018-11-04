c119a8730c5b417777a82a630c01242f8e322e93
adilkhash <adil@playrix.com>
2018-11-04 16:19:26 +0600

    introduced new method get_as_bytes optional arg for get_as_string

--- luigi/contrib/s3.py
+++ luigi/contrib/s3.py
@@ -388,16 +388,18 @@ class S3Client(FileSystem):
         # download the file
         self.s3.meta.client.download_file(bucket, key, destination_local_path)
 
-    def get_as_string(self, s3_path):
+    def get_as_bytes(self, s3_path):
         """
-        Get the contents of an object stored in S3 as a string.
+        Get the contents of an object stored in S3 as bytes
         """
         (bucket, key) = self._path_to_bucket_and_key(s3_path)
-
-        # get the content
         obj = self.s3.Object(bucket, key)
-        contents = obj.get()['Body'].read().decode('utf-8')
+        contents = obj.get()['Body'].read()
         return contents
+
+    def get_as_string(self, s3_path, encoding='utf-8'):
+        content = self.get_as_bytes(s3_path)
+        return content.decode(encoding)
 
     def isdir(self, path):
         """
