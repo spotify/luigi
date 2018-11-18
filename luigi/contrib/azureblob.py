d1ec38edd40731041bfd66a6692017085ca025cc
Manish Sinha <manish.sinha@xamarin.com>
2018-11-17 21:08:08 -0500

    contrib.azureblob: fix docstring generation for progress_callback

--- luigi/contrib/azureblob.py
+++ luigi/contrib/azureblob.py
@@ -242,8 +242,8 @@ class AzureBlobTarget(FileSystemTarget):
             An instance of :class:`luigi.format`.
         :param bool download_when_reading:
             Determines whether the file has to be downloaded to temporary location on disk. Defaults to `True`.
-        :param dict kwargs:
-            Pass the key `progress_callback` with signature `(func(current, total)) ` to get real time progress of upload
+
+        Pass the argument **progress_callback** with signature *(func(current, total))* to get real time progress of upload
         """
         super(AzureBlobTarget, self).__init__(os.path.join(container, blob))
         if format is None:
