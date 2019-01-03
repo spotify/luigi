af6bbacf77477b10fd3bed012595c174f8f8b924
Daniel Huckins <dhuckins@users.noreply.github.com>
2019-01-03 17:37:32 -0500

    Update postgres.py
    added port to `PostgresTarget`

--- luigi/contrib/postgres.py
+++ luigi/contrib/postgres.py
@@ -392,5 +392,6 @@ class PostgresQuery(rdbms.Query):
             user=self.user,
             password=self.password,
             table=self.table,
-            update_id=self.update_id
+            update_id=self.update_id,
+            port=self.port
         )
