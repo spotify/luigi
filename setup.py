99cbec1ce330cf067a3d9d0013959a138aae0533
Mikaela Grundin <9350337+zatine@users.noreply.github.com>
2019-05-06 16:32:13 +0200

    Release 2.8.4

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ if os.environ.get('READTHEDOCS', None) == 'True':
 
 setup(
     name='luigi',
-    version='2.8.3',
+    version='2.8.4',
     description='Workflow mgmgt + task scheduling + dependency resolution',
     long_description=long_description,
     author='The Luigi Authors',
