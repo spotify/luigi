fbe9a4b4e4eef0493a6d4f2495f6371c49fc8edd
Charles Black <18634426+charlesoblack@users.noreply.github.com>
2019-05-24 20:01:59 +0200

    DRY Mail() generation, no retrieval of sendgrid response

--- luigi/notifications.py
+++ luigi/notifications.py
@@ -232,20 +232,21 @@ def send_email_ses(sender, subject, message, recipients, image_png):
 def send_email_sendgrid(sender, subject, message, recipients, image_png):
     import sendgrid as sendgrid_lib
     client = sendgrid_lib.SendGridAPIClient(sendgrid().apikey)
+
+    to_send = sendgrid_lib.Mail(
+            from_email=sender,
+            to_emails=recipients,
+            subject=subject)
+
     if email().format == 'html':
-        to_send = sendgrid_lib.Mail(from_email=sender,
-            to_emails=recipients,
-            subject=subject,
-            html_content=message)
+        to_send.add_content(message,'text/html')
     else:
-        to_send = sendgrid_lib.Mail(from_email=sender,
-            to_emails=recipients,
-            subject=subject,
-            plain_text_content=message)
+        to_send.add_content(message,'text/plain')
+
     if image_png:
         to_send.add_attachment(image_png)
 
-    response = client.send(to_send)
+    client.send(to_send)
 
 
 def _email_disabled_reason():
