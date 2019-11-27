3473382e1ac1d1d632c6d4a139ba87eff076ef73
m.slavoshevski@cian.ru <m.slavoshevski@cian.ru>
2019-11-27 11:36:16 +0300

    MockFile removed

--- luigi/mock.py
+++ luigi/mock.py
@@ -188,9 +188,3 @@ class MockTarget(target.FileSystemTarget):
             return wrapper
         else:
             return self.format.pipe_reader(Buffer(self.fs.get_all_data()[fn]))
-
-
-class MockFile(MockTarget):
-    def __init__(self, *args, **kwargs):
-        warnings.warn("MockFile has been renamed MockTarget", DeprecationWarning, stacklevel=2)
-        super(MockFile, self).__init__(*args, **kwargs)
