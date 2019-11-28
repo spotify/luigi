c27dec1cf89753ccc47ba0b629759bcfccfcf368
m.slavoshevski@cian.ru <m.slavoshevski@cian.ru>
2019-11-28 19:31:24 +0300

    Revert "Fix params hashing (#2540)"
    This reverts commit 2f41f2679b4a256c6e63a52dde941dbc5d8341c2.

--- luigi/configuration/toml_parser.py
+++ luigi/configuration/toml_parser.py
@@ -22,7 +22,6 @@ except ImportError:
     toml = False
 
 from .base_parser import BaseParser
-from ..freezing import recursively_freeze
 
 
 class LuigiTomlParser(BaseParser):
@@ -52,13 +51,6 @@ class LuigiTomlParser(BaseParser):
         for path in config_paths:
             if os.path.isfile(path):
                 self.data = self._update_data(self.data, toml.load(path))
-
-        # freeze dict params
-        for section, content in self.data.items():
-            for key, value in content.items():
-                if isinstance(value, dict):
-                    self.data[section][key] = recursively_freeze(value)
-
         return self.data
 
     def get(self, section, option, default=NO_DEFAULT, **kwargs):
