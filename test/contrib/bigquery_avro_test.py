5227a8fbabfb056daede92c4649316250148ee36
drowoseque <slavoshevskii.mihail@phystech.edu>
2019-12-03 13:52:50 +0300

    no-if-six.py* (#2840)
    * no-if-six.py*
    
    * no unused imports
    
    * Update .travis.yml
    
    * no py27 in travis (#2836)
    
    * no py27 in travis
    
    * py37
    
    * flake8 and docs back
    
    * py36 py37 postgres
    
    * py36-apache
    
    * MixedUnicodeBytes removed
    
    * unused imports removed
    
    * no-if-py3
    
    * space
    
    * space
    
    * MixedUnicodeBytesFormat returned
    
    * postgres toxenv for py3
    
    * postgres toxenv for py3
    
    * postgres toxenv for py3
    
    * \n
    
    * no-unused-import
    
    * test_multiple_workers back

--- test/contrib/bigquery_avro_test.py
+++ test/contrib/bigquery_avro_test.py
@@ -19,7 +19,6 @@
 These are the unit tests for the BigQueryLoadAvro class.
 """
 
-import six
 import unittest
 import avro
 import avro.schema
@@ -41,15 +40,9 @@ class BigQueryAvroTest(unittest.TestCase):
             ]
         }
         """
-        avro_schema = self._parse_schema(schema_json)
+        avro_schema = avro.schema.Parse(schema_json)
         reader = avro.io.DatumReader(avro_schema, avro_schema)
         actual_schema = BigQueryLoadAvro._get_writer_schema(reader)
         self.assertEqual(actual_schema, avro_schema,
                          "writer(s) avro_schema attribute not found")
         # otherwise AttributeError is thrown
-
-    def _parse_schema(self, schema_json):
-        if six.PY2:
-            return avro.schema.parse(schema_json)
-        else:
-            return avro.schema.Parse(schema_json)
