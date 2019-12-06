10c40242cb5d6b113fd233dd57b80dcf99f0f8f8
drowoseque <slavoshevskii.mihail@phystech.edu>
2019-12-06 13:07:26 +0300

    no six create_bound_method (#2855)

--- luigi/six.py
+++ luigi/six.py
@@ -389,8 +389,6 @@ if PY3:
     def get_unbound_function(unbound):
         return unbound
 
-    create_bound_method = types.MethodType
-
     def create_unbound_method(func, cls):
         return func
 
@@ -398,9 +396,6 @@ if PY3:
 else:
     def get_unbound_function(unbound):
         return unbound.im_func
-
-    def create_bound_method(func, obj):
-        return types.MethodType(func, obj, obj.__class__)
 
     def create_unbound_method(func, cls):
         return types.MethodType(func, None, cls)
