42d9b73878e26eca18a9683e90cceee958b830f2
Tashrif Billah <tbillah@bwh.harvard.edu>
2020-12-29 07:23:53 -0500

    make all indents 4 chars (#3024)

--- luigi/tools/deps_tree.py
+++ luigi/tools/deps_tree.py
@@ -9,18 +9,18 @@ Use this to visualize the execution plan in the terminal.
     $ luigi-deps-tree --module foo_complex examples.Foo
     ...
     └─--[Foo-{} (PENDING)]
-       |--[Bar-{'num': '0'} (PENDING)]
-       |  |--[Bar-{'num': '4'} (PENDING)]
-       |  └─--[Bar-{'num': '5'} (PENDING)]
-       |--[Bar-{'num': '1'} (PENDING)]
-       └─--[Bar-{'num': '2'} (PENDING)]
-          └─--[Bar-{'num': '6'} (PENDING)]
-             |--[Bar-{'num': '7'} (PENDING)]
-             |  |--[Bar-{'num': '9'} (PENDING)]
-             |  └─--[Bar-{'num': '10'} (PENDING)]
-             |     └─--[Bar-{'num': '11'} (PENDING)]
-             └─--[Bar-{'num': '8'} (PENDING)]
-                └─--[Bar-{'num': '12'} (PENDING)]
+        |---[Bar-{'num': '0'} (PENDING)]
+        |   |---[Bar-{'num': '4'} (PENDING)]
+        |   └─--[Bar-{'num': '5'} (PENDING)]
+        |---[Bar-{'num': '1'} (PENDING)]
+        └─--[Bar-{'num': '2'} (PENDING)]
+            └─--[Bar-{'num': '6'} (PENDING)]
+                |---[Bar-{'num': '7'} (PENDING)]
+                |   |---[Bar-{'num': '9'} (PENDING)]
+                |   └─--[Bar-{'num': '10'} (PENDING)]
+                |       └─--[Bar-{'num': '11'} (PENDING)]
+                └─--[Bar-{'num': '8'} (PENDING)]
+                    └─--[Bar-{'num': '12'} (PENDING)]
 """
 
 from luigi.task import flatten
@@ -52,10 +52,10 @@ def print_tree(task, indent='', last=True):
     result = '\n' + indent
     if(last):
         result += '└─--'
-        indent += '   '
+        indent += '    '
     else:
-        result += '|--'
-        indent += '|  '
+        result += '|---'
+        indent += '|   '
     result += '[{0}-{1} ({2})]'.format(name, params, is_complete)
     children = flatten(task.requires())
     for index, child in enumerate(children):
