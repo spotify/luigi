e8e9dfb54df488b6e72f154a6d577f0d2dcaa036
Honnix <honnix@users.noreply.github.com>
2021-01-14 11:39:12 +0100

    Update install_start_azurite.sh

--- scripts/ci/install_start_azurite.sh
+++ scripts/ci/install_start_azurite.sh
@@ -6,5 +6,5 @@ echo "$DOCKERHUB_TOKEN" | docker login -u spotifyci --password-stdin
 
 docker pull arafato/azurite
 mkdir -p blob_emulator
-$1/stop_azurite.sh
+$1/stop_azurite.sh || true
 docker run -e executable=blob -d -t -p 10000:10000 -v blob_emulator:/opt/azurite/folder arafato/azurite
