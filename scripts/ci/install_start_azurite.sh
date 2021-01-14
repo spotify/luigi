bb42bda05278bcffd42bdde0bb13a40fedead861
Hongxin Liang <honnix@spotify.com>
2021-01-14 12:14:01 +0100

    Do not fail so untrusted build may work

--- scripts/ci/install_start_azurite.sh
+++ scripts/ci/install_start_azurite.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
-
 echo "$DOCKERHUB_TOKEN" | docker login -u spotifyci --password-stdin
 
 docker pull arafato/azurite
 mkdir -p blob_emulator
-$1/stop_azurite.sh || true
+$1/stop_azurite.sh
 docker run -e executable=blob -d -t -p 10000:10000 -v blob_emulator:/opt/azurite/folder arafato/azurite
