a43404281d285689094ade63a441fa7539f1cf32
Vincent Barbaresi <vincent.barbaresi@datadoghq.com>
2021-01-16 18:16:02 +0100

    use the new official Azurite image

--- scripts/ci/install_start_azurite.sh
+++ scripts/ci/install_start_azurite.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-docker pull arafato/azurite
+AZURITE_IMAGE=mcr.microsoft.com/azure-storage/azurite
+docker pull $AZURITE_IMAGE
 mkdir -p blob_emulator
 $1/stop_azurite.sh
-docker run -e executable=blob -d -t -p 10000:10000 -v blob_emulator:/opt/azurite/folder arafato/azurite
+docker run -e executable=blob -d -t -p 10000:10000 -v blob_emulator:/opt/azurite/folder $AZURITE_IMAGE
