1212dd88d74847a2d312918f059e22a8eeb429b0
Christoph Thiede <christoph.thiede@outlook.de>
2021-04-15 10:17:07 +0000

    postgres.CopyToTable: Robustize resources cleanup
    Make sure to rollback and close connections as well as to release
    cursors when an error occurs during
    CopyToTable.copy(). This can happen, for example, when rows have an
    invalid format or the method is overriden in a subclass.
    
    Tests are added, too (see DailyCopyToTableTest.test_cleanup_on_error()).

--- test/contrib/postgres_test.py
+++ test/contrib/postgres_test.py
@@ -26,12 +26,47 @@ def datetime_to_epoch(dt):
     return td.days * 86400 + td.seconds + td.microseconds / 1E6
 
 
-class MockPostgresCursor(mock.Mock):
+class MockContextManager(mock.Mock):
+
+    def __init__(self, *args, **kwargs):
+        super(MockContextManager, self).__init__(*args, **kwargs)
+        self.context_counter = 0
+        self.all_context_counter = 0
+
+    def __enter__(self):
+        self.context_counter += 1
+        self.all_context_counter += 1
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.context_counter -= 1
+
+    def _get_child_mock(self, /, **kwargs):
+        """Child mocks will be instances of super."""
+        return mock.Mock(**kwargs)
+
+
+class MockPostgresConnection(MockContextManager):
+    def __init__(self, existing_update_ids, *args, **kwargs):
+        super(MockPostgresConnection, self).__init__(*args, **kwargs)
+        self.existing = existing_update_ids
+        self.is_open = False
+        self.was_open = 0
+
+    def cursor(self):
+        self.is_open = True
+        self.was_open = True
+        return MockPostgresCursor(existing_update_ids=self.existing)
+
+    def close(self):
+        self.is_open = False
+
+
+class MockPostgresCursor(MockContextManager):
     """
     Keeps state to simulate executing SELECT queries and fetching results.
     """
-    def __init__(self, existing_update_ids):
-        super(MockPostgresCursor, self).__init__()
+    def __init__(self, existing_update_ids, *args, **kwargs):
+        super(MockPostgresCursor, self).__init__(*args, **kwargs)
         self.existing = existing_update_ids
 
     def execute(self, query, params):
@@ -81,6 +116,44 @@ class DailyCopyToTableTest(unittest.TestCase):
             DummyPostgresImporter(date=datetime.datetime(2015, 1, 6)).task_id,
         ]))
         self.assertFalse(task.complete())
+
+    @mock.patch('psycopg2.connect')
+    @mock.patch("luigi.contrib.postgres.CopyToTable.rows", return_value=['row1', 'row2'])
+    def test_cleanup_on_error(self, mock_rows, mock_connect):
+        """
+        Test cleanup behavior of CopyToTable in case of an error.
+
+        When an error occured while the connection is open, it should be
+        closed again so that subsequent tasks do not fail due to the unclosed
+        connection.
+        """
+        task = DummyPostgresImporter(date=datetime.datetime(2021, 4, 15))
+
+        mock_connection = MockPostgresConnection([task.task_id])
+        mock_connect.return_value = mock_connection
+        mock_cursor = MockPostgresCursor([task.task_id])
+
+        original_cursor = mock_connection.cursor
+        def get_mock_cursor():
+            original_cursor()
+            return mock_cursor
+
+        mock_connection.cursor = mock.MagicMock(side_effect=get_mock_cursor)
+
+        task = DummyPostgresImporter(date=datetime.datetime(2021, 4, 15))
+        task.columns = [(42,)]  # inject defect
+
+        #import pdb; pdb.set_trace()
+        with self.assertRaisesRegex(ValueError, "columns"):
+            task.run()
+
+        self.assertEqual(mock_connection.context_counter, 0)
+        self.assertTrue(mock_connection.all_context_counter)
+        self.assertFalse(mock_connection.is_open)
+        self.assertTrue(mock_connection.was_open)
+
+        self.assertEqual(mock_cursor.context_counter, 0)
+        self.assertTrue(mock_cursor.all_context_counter)
 
 
 class DummyPostgresQuery(luigi.contrib.postgres.PostgresQuery):
