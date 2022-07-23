4839b05211c5d2762360985ecbdb6178fb7137fc
Ikko Ashimine <eltociear@gmail.com>
2022-07-24 01:21:16 +0900

    Fix typo in interface.py
    programatically -> programmatically

--- luigi/interface.py
+++ luigi/interface.py
@@ -18,7 +18,7 @@
 This module contains the bindings for command line integration and dynamic loading of tasks
 
 If you don't want to run luigi from the command line. You may use the methods
-defined in this module to programatically run luigi.
+defined in this module to programmatically run luigi.
 """
 
 import logging
