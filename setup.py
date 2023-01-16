b6ff4c0a9134d79f4fb244470de7e1c7c706c805
Adrien Berchet <adrien.berchet@gmail.com>
2023-01-16 16:44:12 +0100

    Can pass a JSON schema to DictParameter and ListParameter (#3217)
    * Can pass a JSON schema to DictParameter
    
    * Add schema to ListParameter
    
    * Fix flake8
    
    * Fix doc generation
    
    * Make jsonschema optional but raise a warning if missing and a schema is given
    
    * Add tests for warnings
    
    * Flake8

--- setup.py
+++ setup.py
@@ -100,6 +100,7 @@ setup(
     },
     install_requires=install_requires,
     extras_require={
+        'jsonschema': ['jsonschema'],
         'prometheus': ['prometheus-client>=0.5,<0.15'],
         'toml': ['toml<2.0.0'],
     },
