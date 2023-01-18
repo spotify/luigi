4fbdbee6f8e207f4f3ea601874b064d955b9c369
ROUDET Franck INNOV/IT-S <EFRE6460@orange.com>
2023-01-18 16:52:18 +0100

    Add test for not root default_scheduler_url
    Signed-off-by: ROUDET Franck INNOV/IT-S <EFRE6460@orange.com>

--- test/rpc_test.py
+++ test/rpc_test.py
@@ -37,6 +37,13 @@ class RemoteSchedulerTest(unittest.TestCase):
                 with mock.patch.object(s, '_fetcher') as fetcher:
                     s._fetch(suffix, '{}')
                     fetcher.fetch.assert_called_once_with('http://zorg.com/api/123', '{}', 42)
+    def testUrlArgumentVariationsNotRoot(self):
+        for url in ['http://zorg.com/subpath', 'http://zorg.com/subpath/']:
+            for suffix in ['api/123', '/api/123']:
+                s = luigi.rpc.RemoteScheduler(url, 42)
+                with mock.patch.object(s, '_fetcher') as fetcher:
+                    s._fetch(suffix, '{}')
+                    fetcher.fetch.assert_called_once_with('http://zorg.com/subpath/api/123', '{}', 42)
 
     def get_work(self, fetcher_side_effect):
         scheduler = luigi.rpc.RemoteScheduler('http://zorg.com', 42)
