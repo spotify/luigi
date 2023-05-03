8751da3499b4625db68cf30bd395b38ae0f1f302
sonjaer <sonjae@spotify.com>
2023-05-03 16:55:57 +0200

    Update test/contrib/bigquery_client_test.py
    Co-authored-by: Honnix <honnix@users.noreply.github.com>

--- test/contrib/bigquery_client_test.py
+++ test/contrib/bigquery_client_test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2019 Spotify AB
+# Copyright 2023 Spotify AB
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
