b5be1474ce8e9dbcf2c8fbb5352d9faaea5056e1
Jumpei Ukita <ukky17@users.noreply.github.com>
2023-12-18 01:24:46 +0900

    Add python 3.11 support (#3265)
    * add python 3.11 support
    * use getfullargspec instead of getargspec
    
    ---------
    
    Co-authored-by: jumpei-ukita <jumpei-ukita@m3.com>

--- setup.py
+++ setup.py
@@ -117,6 +117,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: System :: Monitoring',
     ],
 )
