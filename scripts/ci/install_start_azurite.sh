e40a4b40568ba9a1772da6a8792dfd0a73bc222f
Matthias Siemering <msiemeri@posteo.net>
2024-05-31 11:12:45 +0200

    Updated luigi.contrib.azureblob to 12.x.y series of azure.storage.blob

--- scripts/ci/install_start_azurite.sh
+++ scripts/ci/install_start_azurite.sh
@@ -5,4 +5,4 @@ echo "$DOCKERHUB_TOKEN" | docker login -u spotifyci --password-stdin
 docker pull mcr.microsoft.com/azure-storage/azurite
 mkdir -p blob_emulator
 $1/stop_azurite.sh
-docker run -p 10000:10000 -v blob_emulator:/data -d mcr.microsoft.com/azure-storage/azurite azurite-blob -l /data --blobHost 0.0.0.0 --blobPort 10000
+docker run -p 10000:10000 -v blob_emulator:/data -e AZURITE_ACCOUNTS=devstoreaccount1:YXp1cml0ZQ== -d mcr.microsoft.com/azure-storage/azurite azurite-blob -l /data --blobHost 0.0.0.0 --blobPort 10000
