daaf37c35901d6c401db1b672c206cce7772a80e
mrafayaleem <mohammad.aleem@affirm.com>
2026-01-16 17:03:43 +0000

    Add removed tests

--- test/server_test.py
+++ test/server_test.py
@@ -97,6 +97,17 @@ class ServerTest(ServerTestBase):
     def test_api_404(self):
         self._test_404('/api/foo')
 
+    def test_api_cors_headers(self):
+        response = self.fetch('/api/graph')
+        headers = dict(response.headers)
+
+        def _set(name):
+            return set(headers[name].replace(" ", "").split(","))
+
+        self.assertSetEqual(_set("Access-Control-Allow-Headers"), {"Content-Type", "Accept", "Authorization", "Origin"})
+        self.assertSetEqual(_set("Access-Control-Allow-Methods"), {"GET", "OPTIONS"})
+        self.assertEqual(headers["Access-Control-Allow-Origin"], "*")
+
 
 class _ServerTest:
     """
@@ -148,12 +159,12 @@ class _ServerTest:
 
     @skipOnTravis('https://travis-ci.org/spotify/luigi/jobs/72953884')
     def test_save_state(self):
-        self.sch.add_task('X', 'B', deps=('A',))
-        self.sch.add_task('X', 'A')
-        self.assertEqual(self.sch.get_work('X')['task_id'], 'A')
+        self.sch.add_task(worker='X', task_id='B', deps=('A',))
+        self.sch.add_task(worker='X', task_id='A')
+        self.assertEqual(self.sch.get_work(worker='X')['task_id'], 'A')
         self.stop_server()
         self.start_server()
-        work = self.sch.get_work('X')['running_tasks'][0]
+        work = self.sch.get_work(worker='X')['running_tasks'][0]
         self.assertEqual(work['task_id'], 'A')
 
 
